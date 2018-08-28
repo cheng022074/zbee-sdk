b7f614bc09badd8e09a5ceeb6b02814fa04a0e32
DESKTOP-BSUQOB6\nero.chen <cheng022074@qq.com>
2018-08-28 10:28:54 +0800

    ES5版本HTTP请求机制

--- src/browser/es5/http/requst.fn.js
+++ src/browser/es5/http/requst.fn.js
@@ -2,13 +2,66 @@
  * 
  * 向后台应用服务发送请求
  * 
+ * @import parse from http.params.parse
+ * 
+ * @import empty from function.empty
+ * 
+ * @import isObject from is.object.simple
+ * 
  * @param {string} uri 服务请求链接
  * 
- * @param {string} [method = 'GET'] 请求方式，默认 GET 请求
+ * @param {string} [method] 请求方式
  * 
- * @param {object} [params = {}] 服务请求数据
+ * @param {object} [params] 服务请求数据
  * 
  */
 
+// 为了简化与实用原则，当前函数未实现非JSON机制
 
+const {
+    url,
+    headers,
+    method,
+    timeout,
+    body
+} = parse(uri , method , params),
+http = new XMLHttpRequest(),
+{
+    stringify,
+    parse
+} = JSON,
+successFn = empty,
+failureFn = empty;
 
+if(isObject(params)){
+
+    if(params.hasOwnProperty('success')){
+
+        successFn = params.success ;
+    }
+
+    if(params.hasOwnProperty('failure')){
+
+        failureFn = params.failure ;
+    }
+}
+
+http.addEventListener('readystatechange' , () =>{
+
+    if(http.readyState === 4){
+
+        if(http.status === 200){
+
+            successFn(parse(http.responseText)) ;
+
+        }else{
+
+            failureFn() ;
+        }
+    }
+
+}) ;
+
+http.open(method , url , true) ;
+
+http.send(stringify(body)) ;
--- src/http/methods/put.fn.js
+++ src/http/methods/put.fn.js
@@ -14,4 +14,3 @@
  */
 
 return request(uri , 'put' , params) ;
-
