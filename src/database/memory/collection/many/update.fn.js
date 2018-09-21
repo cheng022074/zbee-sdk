bc3538d268e584e3b3e6b28184ed4cfc5b0f0f38
陈治文 <cheng022074@qq.com>
2018-09-21 21:41:10 +0800

    no message

---
+++ src/database/memory/collection/many/update.fn.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * 更新一批数据
+ * 
+ * 
+ * @param {object} filter 更新条件
+ * 
+ * @param {object} updateDoc 更新的数据
+ * 
+ * 
+ * @return {Promise} 
+ * 
+ */
+
+ // 代码实现
---
+++ src/database/memory/collection/one/find.fn.js
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * 查询一条记录
+ * 
+ * 
+ * @param {object} query 查询条件
+ * 
+ * @param {object} options 查询配置
+ * 
+ * @param {object} [options.skip = 0] 查询起始位置
+ * 
+ * @param {object} [options.limit = 0] 查询总数
+ * 
+ * @param {object} [options.projection] 查询字段
+ * 
+ * 
+ * @return {Promise} 
+ * 
+ */
+
+ // 代码实现
