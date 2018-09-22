4adaa97eadb63d136b317e6abe1c19248ccd95c2
陈治文 <cheng022074@qq.com>
2018-09-22 12:53:55 +0800

    no message

---
+++ src/database/memory/collection/sync/read.fn.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * 读取同步数据文件中的数据内容
+ * 
+ * @import getSyncFilePath from database.memory.collection.sync.path
+ * 
+ * @param {string} name 数据存储表名
+ * 
+ * @param {string} path 数据存储路径
+ * 
+ */
+
+read(getSyncFilePath(name , path)) || [];
