0cd95ea44f84713c3835e9f6d7f91b907743fd69
陈治文 <cheng022074@qq.com>
2018-11-01 21:34:15 +0800

    no message

--- package/vue-toolkit/index.js
+++ package/vue-toolkit/index.js
@@ -1,6 +1,105 @@
 
 
 
+exports['css::var.reset'] = `$var-reset-font-family:"Helvetica Neue", HelveticaNeue, "Helvetica-Neue", Helvetica, "BBAlpha Sans", sans-serif !default;`
+
+
+exports['css::reset'] = `html, body {
+    font-family: $var-reset-font-family;
+    font-weight: normal;
+    text-size-adjust: none;
+    margin: 0;
+    cursor: default;
+}
+
+body, div, dl, dt, dd, ul, ol, li, h1, h2, h3,
+h4, h5, h6, pre, code, form, fieldset, legend,
+input, textarea, p, blockquote, th, td {
+    margin: 0;
+    padding: 0;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+
+fieldset, img {
+    border: 0;
+}
+
+address, caption, cite, code, dfn, em, strong, th, var {
+    font-style: normal;
+    font-weight: normal;
+}
+
+li {
+    list-style: none;
+}
+
+caption, th {
+    text-align: left;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    font-size: 100%;
+    font-weight: normal;
+}
+
+q:before,
+q:after {
+    content: '';
+}
+
+abbr, acronym {
+    border: 0;
+    font-variant: normal;
+}
+
+sup {
+    vertical-align: text-top;
+}
+
+sub {
+    vertical-align: text-bottom;
+}
+
+input, textarea, select {
+    font-family: inherit;
+    font-size: inherit;
+    font-weight: inherit;
+}
+
+*:focus {
+    outline: none;
+}
+
+body.x-desktop {
+    overflow: hidden;
+}
+
+
+*, *:after, *:before {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    -webkit-touch-callout: none;
+    -webkit-user-drag: none;
+    user-select: none;
+    touch-action: none;
+}
+
+input, textarea {
+    user-select: text;
+}`
+
+
+exports['css::reset.viewport'] = `html,body {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}`
+
+
 exports['src::is.type'] = (() =>{
     
     
@@ -49,7 +148,7 @@ exports['src::is.array'] = (() =>{
     
     
     
-    let __first_executed_1541049549322__ = false ;
+    let __first_executed_1541079163964__ = false ;
     
 
     
@@ -61,10 +160,10 @@ exports['src::is.array'] = (() =>{
     }
     return function(data){
         
-        if(!__first_executed_1541049549322__){
+        if(!__first_executed_1541079163964__){
             isType = include('is.type');
             
-            __first_executed_1541049549322__ = true ;
+            __first_executed_1541079163964__ = true ;
         }
         
         
@@ -91,47 +190,6 @@ exports['src::is.array'] = (() =>{
 })() ;
 
 
-exports['is.type'] = (() =>{
-    
-    
-    
-    
-    
-    
-
-    
-    function main(data,type){
-
-        
-
- return typeof data === type ;
-    }
-    return function(data,type){
-        
-        
-        return  main.call((function(){
-
-            let me = this,
-                target;
-
-            if(typeof global !== 'undefined'){
-
-                target = global ;
-            
-            }else{
-
-                target = window ;
-            }
-
-            return me === target ? main : me ;
-
-        }).call(this) , data,type) ;
-    }
-    
-
-})() ;
-
-
 exports['src::is.empty'] = (() =>{
     let isArray;
     
@@ -139,7 +197,7 @@ exports['src::is.empty'] = (() =>{
     
     
     
-    let __first_executed_1541049549322__ = false ;
+    let __first_executed_1541079163964__ = false ;
     
 
     
@@ -151,10 +209,10 @@ return (data == null) || (!allowEmptyString ? data === '' : false) || (isArray(d
     }
     return function(data,allowEmptyString = false){
         
-        if(!__first_executed_1541049549322__){
+        if(!__first_executed_1541079163964__){
             isArray = include('is.array');
             
-            __first_executed_1541049549322__ = true ;
+            __first_executed_1541079163964__ = true ;
         }
         
         
@@ -181,14 +239,14 @@ return (data == null) || (!allowEmptyString ? data === '' : false) || (isArray(d
 })() ;
 
 
-exports['is.array'] = (() =>{
+exports['src::is.string'] = (() =>{
     let isType;
     
     
     
     
     
-    let __first_executed_1541049549322__ = false ;
+    let __first_executed_1541079163964__ = false ;
     
 
     
@@ -196,14 +254,14 @@ exports['is.array'] = (() =>{
 
         
 
- return Array.isArray(data) ;
+return isType(data , 'string') ;
     }
     return function(data){
         
-        if(!__first_executed_1541049549322__){
+        if(!__first_executed_1541079163964__){
             isType = include('is.type');
             
-            __first_executed_1541049549322__ = true ;
+            __first_executed_1541079163964__ = true ;
         }
         
         
@@ -230,14 +288,14 @@ exports['is.array'] = (() =>{
 })() ;
 
 
-exports['src::is.string'] = (() =>{
-    let isType;
+exports['src::array.from'] = (() =>{
+    let isEmpty,isString;
     
     
     
     
     
-    let __first_executed_1541049549323__ = false ;
+    let __first_executed_1541079163964__ = false ;
     
 
     
@@ -245,14 +303,28 @@ exports['src::is.string'] = (() =>{
 
         
 
-return isType(data , 'string') ;
+if(isEmpty(data)){
+
+    return [];
+}
+
+if (data && data.length !== undefined && !isString(data)) {
+
+    return Array.from(data);
+
+}
+
+return [
+    data
+];
     }
     return function(data){
         
-        if(!__first_executed_1541049549323__){
-            isType = include('is.type');
+        if(!__first_executed_1541079163964__){
+            isEmpty = include('is.empty');
+isString = include('is.string');
             
-            __first_executed_1541049549323__ = true ;
+            __first_executed_1541079163964__ = true ;
         }
         
         
@@ -273,6 +345,47 @@ return isType(data , 'string') ;
             return me === target ? main : me ;
 
         }).call(this) , data) ;
+    }
+    
+
+})() ;
+
+
+exports['src::path.string'] = (() =>{
+    
+    
+    
+    
+    
+    
+
+    
+    function main(path){
+
+        
+
+return path.replace(/\\/g , '\\\\') ;
+    }
+    return function(path){
+        
+        
+        return  main.call((function(){
+
+            let me = this,
+                target;
+
+            if(typeof global !== 'undefined'){
+
+                target = global ;
+            
+            }else{
+
+                target = window ;
+            }
+
+            return me === target ? main : me ;
+
+        }).call(this) , path) ;
     }
     
 
