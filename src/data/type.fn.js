74fa5198851daaa3f6a21c6c154d05ad353d1a32
陈治文 <cheng022074@qq.com>
2018-11-30 22:02:17 +0800

    no message

--- src/data/type.fn.js
+++ src/data/type.fn.js
@@ -5,9 +5,7 @@
  * 
  * @import data.type.auto
  * 
- * @import data.type.string
- * 
- * @param {string} name 数据类型名称
+ * @param {string} [name = 'auto'] 数据类型名称
  * 
  * @return {data.Type} 数据类型引用
  * 
