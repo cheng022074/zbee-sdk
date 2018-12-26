96993d1a7c85534f0f92cf18fcd27c4997b6c864
陈治文 <cheng022074@qq.com>
2018-12-26 10:13:08 +0800

    字符串转换

---
+++ src/string/to/boolean.fn.js
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * 将字符串转换成布尔型
+ * 
+ * @param {string} data 字符串数据
+ * 
+ * @return {boolean} 布尔型 
+ * 
+ */
+
+switch(data){
+
+    case 'yes':
+    case 'true':
+
+        return true ;
+
+    case 'no':
+    case 'false':
+
+        return false ;
+}
---
+++ src/string/to/number.fn.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * 将字符串转换成数字
+ * 
+ * @param {string} data 字符串数据
+ * 
+ * @return {number} 数字  
+ * 
+ */
+
+return parseInt(data , 10) ;
