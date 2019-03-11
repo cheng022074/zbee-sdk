924bb9e2b9354bccd9dc431c3c10a7c9cf13cc80
陈治文 <cheng022074@qq.com>
2019-03-11 16:41:13 +0800

    AJAX 查询

---
+++ src/data/connection/ajax.fn.js
@@ -0,0 +1,60 @@
+
+/**
+ * 
+ * 基于 AJAX 进行数据交互
+ * 
+ * @import append from url.append
+ * 
+ * @import apply from url.template.apply
+ * 
+ * @param {string} url 请求路径
+ * 
+ * @param {object} [config] 请求配置
+ * 
+ * @param {string} [config.method = 'GET'] 请求方式，默认是 GET 请求
+ * 
+ * @param {object} [config.query] GET请求的参数集合
+ * 
+ * @param {object} [config.params] 请求主体的参数集合
+ * 
+ * @param {object} [config.path] 以路径参数形式提交的参数集合
+ * 
+ * @param {boolean} [config.json = true] 是否以 JSON方式提交数据  
+ * 
+ */
+
+if(query){
+
+    url = append(url , query) ;
+}
+
+if(path){
+
+    url = apply(url , path) ;
+}
+
+const axios = require('axios'),
+{
+    stringify
+} = require('querystring');
+
+switch(method){
+
+    case 'GET':
+    case 'DELETE':
+
+        url = append(url , params) ;
+
+        break ;
+
+    case 'POST':
+    case 'PUT':
+
+        if(json === false){
+
+            params = stringify(params) ;
+        }
+}
+
+axios[method.toLowerCase()](url , params) ;
+
