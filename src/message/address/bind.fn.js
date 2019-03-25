e7d8908856f7c67af3cc0e2d9d9e9ddab9557e18
陈治文 <cheng022074@qq.com>
2019-03-25 13:36:24 +0800

    基于 React 的消息机

--- src/message/address/bind.fn.js
+++ src/message/address/bind.fn.js
@@ -27,4 +27,8 @@
 
   me.target = target ;
 
+  return true ;
+
  }
+
+ return false ;
---
+++ src/message/address/react.fn.js
@@ -0,0 +1,65 @@
+/**
+ * 
+ * 基于 React 基于消息系统
+ * 
+ * @import getCenter from message.center
+ * 
+ * @param {React.Component} Target 继承的 React 组件类
+ * 
+ * @param {string} [address] 组件注册的消息地址
+ * 
+ */
+
+ let center = getCenter() ;
+
+ return class extends Target{
+
+    componentDidMount() {
+
+        let me = this,
+        {
+            address:currentAddress
+        } = this.props ;
+ 
+        currentAddress = currentAddress || address ;
+
+        if(!currentAddress){
+
+            throw new Error(`${Target.name} 未注册消息地址`) ;
+        }
+
+        let messageAddress = center.register(currentAddress) ;
+
+        let isBind = messageAddress.bind(me) ;
+
+        if(!isBind){
+
+            throw new Error(`${Target.name} 尝试注册已被占用的消息地址 ${currentAddress}`) ;
+        }
+
+        me.$address = messageAddress ;
+
+        if (super.componentDidMount) {
+
+            super.componentDidMount();
+        }
+    }
+
+    componentWillUnMount() {
+
+        if (super.componentWillUnMount) {
+
+            super.componentWillUnMount();
+        }
+
+        let {
+            $address:address
+        } = this ;
+
+        if(address){
+
+            address.unbind() ;
+        }
+    }
+
+ }
