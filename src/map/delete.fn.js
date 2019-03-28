147013cf90956e15db00a99f02385e52eec9cc1b
陈治文 <cheng022074@qq.com>
2019-03-28 11:06:17 +0800

    删除指定键值

--- src/map/delete.fn.js
+++ src/map/delete.fn.js
@@ -1,12 +1,30 @@
 
+
 /**
  * 
- * 函数实现说明
+ * 删除指定组合键
  * 
- * @param {mixed} data 参数说明
+ * @import find from .find scoped
  * 
- * @return {mixed} 返回说明 
+ * @param {array} [...keys] 组合键
+ * 
+ * @return {boolean} 如果组合键存在，则返回 true , 否则返回 false 
  * 
  */
 
- // 代码实现
+let me = this,
+{
+    map
+} = me ;
+
+let {
+    match,
+    key
+} = find(keys) ;
+
+if(match){
+
+    return map.delete(key) ;
+}
+
+return false ;
