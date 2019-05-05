e0ae9faf3e031eb53763ce82602ad90beaca9d27
陈治文 <cheng022074@qq.com>
2019-05-05 09:15:13 +0800

    对于数据模型与数据存储器的判断

---
+++ src/is/data/model.fn.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * 判定数据是否为数据模型对象
+ * 
+ * @import Model from data.model value
+ * 
+ * @param {mixed} data 检测数据
+ * 
+ * @return {boolean} 如果检测为数据模型则返回 true , 否则返回 false 
+ * 
+ */
+
+ return data instanceof Model ;
