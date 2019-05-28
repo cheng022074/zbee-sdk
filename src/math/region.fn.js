ed82873919f567d8beaa339f0f962933d52985e6
陈治文 <cheng022074@qq.com>
2019-05-28 18:24:18 +0800

    xxxxxxxxx

---
+++ src/math/region.fn.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * 区域
+ * 
+ * @param {object} config 配置
+ * 
+ * @param {number} [config.x] 横坐标
+ * 
+ * @param {number} [config.y] 纵坐标
+ * 
+ * @param {number} [config.width] 宽度
+ * 
+ * @param {number} [config.height] 高度
+ * 
+ */
+
+ class main{
+
+    constructor(){
+
+        
+    }
+ }
+
+ 
