217399313fd88cb9a973c58bfa65ed5640b5d8bb
陈治文 <cheng022074@qq.com>
2019-05-29 16:58:00 +0800

    垂直方向选择

---
+++ src/data/model/node/tree/down.fn.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * 向下选择一个节点
+ * 
+ * @import select from .select.vertical scoped
+ * 
+ */
+
+ select('down') ;
+
+ 
---
+++ src/data/model/node/tree/up.fn.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 向上选择一个节点
+ * 
+ * @import select from .select.vertical scoped
+ * 
+ */
+
+select('up') ;
