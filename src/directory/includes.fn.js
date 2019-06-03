11f6d08c5434bf369edfe94e610a147bc5bfc091
陈治文 <cheng022074@qq.com>
2019-06-03 10:52:46 +0800

    路径配置

---
+++ src/directory/includes.fn.js
@@ -0,0 +1,91 @@
+
+/**
+ * 
+ * 在指定目录下包含下符合包含、排除规则的所有文件
+ * 
+ * @import from from array.from
+ * 
+ * @import is.file
+ * 
+ * @import is.directory
+ * 
+ * @import getFilePaths from ..readAllFilePaths
+ * 
+ * @import ext from path.ext
+ * 
+ * @param {string} path 指定目录路径
+ * 
+ * @param {object} options 配置
+ * 
+ * @param {mixed} options.includes 包含资源
+ * 
+ * @param {mixed} [options.excludes] 排除资源
+ * 
+ * @param {mixed} [options.suffixes] 文件后缀名
+ * 
+ * @return {array} 所有符合规则的文件
+ * 
+ */
+
+const {
+    join
+} = require('path');
+
+function main(path , {
+    includes,
+    excludes,
+    suffixes
+}){
+
+    let includeFilePaths = getPaths(includes),
+        excludeFilePaths = getPaths(excludes);
+
+    if(suffixes){
+
+        suffixes = from(suffixes) ;
+    }
+    
+    return includeFilePaths.filter(path => {
+
+        if(!excludeFilePaths.includes(path)){
+
+            if(suffixes && !suffixes.includes(ext(path))){
+
+                return false ;
+            }
+
+            return true ;
+        }
+
+        return false ;
+
+    }) ;
+}
+
+function getPaths(path , resources){
+
+    resources = from(resources) ;
+
+    let resourcePaths = [] ;
+
+    for(let resource of resources){
+
+        let resourcePath = join(path , resource) ;
+    
+        if(isFile(resourcePath)){
+    
+            resourcePaths.push(resourcePath) ;
+    
+        }else if(isDirectory(resourcePath)){
+    
+            resourcePaths.push(...getFilePaths(resourcePath)) ;
+        }
+    }
+
+    return resourcePaths ;
+    
+}
+
+
+
+
--- src/directory/readAllFilePaths.fn.js
+++ src/directory/readAllFilePaths.fn.js
@@ -9,7 +9,7 @@
  * 
  * @param {string} path 文件夹目录路径
  * 
- * @param {RegExp} [testRe = /^.+$/] 路径匹配正则表达式
+ * @param {RegExp} [testRe] 路径匹配正则表达式
  * 
  * @return {array} 多个文件路径
  * 
@@ -33,7 +33,12 @@ function main(path , testRe){
     
             let targetPath = join(path , name) ;
     
-            if(isFile(targetPath) && testRe.test(targetPath)){
+            if(isFile(targetPath)){
+
+                if(testRe && !testRe.test(targetPath)){
+
+                    continue ;
+                }
     
                 paths.push(targetPath) ;
             
