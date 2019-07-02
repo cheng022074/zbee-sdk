1072edb12ede9846e9ea6d78af4d5ccfe5c387a8
陈治文 <cheng022074@qq.com>
2019-07-02 11:32:33 +0800

    批量取消订阅

--- src/data/connection/unsubscribes.fn.js
+++ src/data/connection/unsubscribes.fn.js
@@ -3,31 +3,13 @@
  * 
  * 批量取消订阅
  * 
- * @param {object} config 批量订阅配置
+ * @param {string[]} names 批量订阅配置
  * 
  */
 
-let {
-   scope,
-   ...subscribers
-} = config,
-me = this,
-names = Object.keys(subscribers);
+let me = this;
 
 for(let name of names){
 
-   let target = subscribers[name];
-
-   if(isString(target)){
-
-       subscriber = me.unsubscribe(name , target , scope) ;
-
-   }else if(isObject(target)){
-
-       let {
-           fn
-       } = target ;
-
-       subscriber = me.unsubscribe(name , fn , scope) ;
-   }
+    me.unsubscribe(name) ;
 }
