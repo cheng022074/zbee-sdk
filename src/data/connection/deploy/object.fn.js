400544b1a41cc45937422f8c0e36b0be2cf979e8
陈治文 <cheng022074@qq.com>
2019-07-03 15:13:10 +0800

    xxx

--- src/data/connection/deploy/object.fn.js
+++ src/data/connection/deploy/object.fn.js
@@ -24,7 +24,7 @@
     if(subscribers){
 
         config[name] ={
-            varName:`$${name}`,
+            varName:`$${field}`,
             connection:connections[name],
             subscribers
         } ;
