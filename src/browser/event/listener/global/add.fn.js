edc947b757c8f80c740a68d8722ffe5c5f561c01
陈治文 <cheng022074@qq.com>
2019-07-31 15:06:02 +0800

    全局注册事件监听

--- src/browser/event/listener/global/add.fn.js
+++ src/browser/event/listener/global/add.fn.js
@@ -1,12 +1,42 @@
 
 /**
  * 
- * 函数实现说明
+ * 监听全局事件
  * 
- * @param {mixed} data 参数说明
+ * @import getListeners from ..browser.event.listeners
+ * 
+ * @import is from browser.selector.parent
+ * 
+ * @param {string} event 目标监听事件
+ * 
+ * @param {function} fn 目标监听回调
+ * 
+ * @param {string} selector 选择器
  * 
  * @return {mixed} 返回说明 
  * 
  */
 
- // 代码实现
+let listeners = getListeners(),
+    listenerFn = e =>{
+
+        let {
+            target
+        } = e ;
+
+        if(selector){
+
+            if(is(target , selector)){
+
+                fn(e) ;
+            }
+            
+        }else{
+
+            fn(e) ;
+        }
+    };
+
+map.set(event , fn , selector , listenerFn) ;
+
+window.addEventListener(event , listenerFn) ;
--- src/browser/event/listener/global/remove.fn.js
+++ src/browser/event/listener/global/remove.fn.js
@@ -1,12 +1,24 @@
 
 /**
  * 
- * 函数实现说明
+ * 去除监听全局事件
  * 
- * @param {mixed} data 参数说明
+ * @import getListeners from ..browser.event.listeners
+ * 
+ * @param {string} event 目标监听事件
+ * 
+ * @param {function} fn 目标监听回调
+ * 
+ * @param {string} selector 选择器
  * 
  * @return {mixed} 返回说明 
  * 
  */
 
- // 代码实现
+let listeners = getListeners(),
+listenerFn = listeners.get(event , fn , selector);
+
+if(listenerFn){
+
+    window.removeEventListener(event , listenerFn) ;
+}
