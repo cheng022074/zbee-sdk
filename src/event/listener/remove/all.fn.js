dcc8413919f3e5258fa02c8b8ae67fe6aea23037
陈治文 <cheng022074@qq.com>
2019-08-18 10:38:55 +0800

    可去除全部事件监听

---
+++ src/event/listener/remove/all.fn.js
@@ -0,0 +1,21 @@
+
+/**
+ * 
+ * 去除所有事件监听
+ * 
+ * @import listeners from ....listeners value
+ * 
+ * @import remove from ....remove
+ * 
+ * @param {mixed} target 事件主体
+ * 
+ */
+
+ let result = listeners.find(target) ;
+
+ for(let {
+     key
+ } of result){
+
+    remove(target , key[1] , key[2]) ;
+ }
