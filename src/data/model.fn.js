3c3476f2dfc82c7f5a49e95bbab7f4ad9ba07cc0
陈治文 <cheng022074@qq.com>
2019-10-06 08:08:46 +0800

    xxxxxx

--- src/data/model.fn.js
+++ src/data/model.fn.js
@@ -3,6 +3,8 @@
  * 
  * 数据模型类
  * 
+ * @import createReader from data.reader.create
+ * 
  * @param {object} model 数据模型定义
  * 
  * @return {data.Model} 数据模型对象 
@@ -13,11 +15,25 @@
 
     constructor(model){
 
+        let me = this ;
 
+        me.reader = createReader(model) ;
+
+        me.data = {} ;
     }
 
     load(data){
 
-        
+        let me = this,
+        {
+            reader
+        } = me ;
+
+        let result = reader.read(data) ;
+
+        if(result.length){
+
+            me.data = result[0] ;
+        }
     }
  }
--- src/data/store.fn.js
+++ src/data/store.fn.js
@@ -3,6 +3,10 @@
  * 
  * 数据存储器类
  * 
+ * @import createReader from data.reader.create
+ * 
+ * @import createData from data.outer.create.array
+ * 
  * @param {object} model 数据模型定义
  * 
  * @return {data.Model} 数据存储器对象 
@@ -13,11 +17,22 @@ class main{
 
     constructor(model){
 
+        let me = this ;
 
+        me.reader = createReader(model) ;
+
+        me.data = [] ;
+
+        me.outerData = createData(this , model) ;
     }
 
     load(data){
 
-        
+        let me = this,
+        {
+            reader
+        } = me ;
+
+        me.data = reader.read(data) ;
     }
  }
