37a12b639820a3679950f6320612efa0d54bfe61
陈治文 <cheng022074@qq.com>
2019-10-08 17:01:43 +0800

    可钻取解析数据，支持 mapping、model、multi、set、get 属性

--- src/data/reader/create.fn.js
+++ src/data/reader/create.fn.js
@@ -1,21 +1,116 @@
-
 /**
  * 
  * 生成数据读取器
+ * 
+ * @import get from object.value.get
+ * 
+ * @import from from array.from
+ * 
+ * @import is.string
+ * 
+ * @import isObject from is.object.simple
+ * 
+ * @import emptyFn from function.empty value
+ * 
+ * @import is.function
+ * 
+ * @import createReader from data.reader.create
  * 
  * @param {object} model 数据模型定义
  * 
  * @param {string} [model.root = '.'] 数据读取根路径
  * 
+ * @param {object} [model.properties = {}] 数据字段定义
+ * 
  * @return {data.Reader} 数据模型对象 
  * 
  */
 
- const codes = [
-     'var get = include("object.value.get") ;',
-     'var from = include("array.from");'
- ] ;
+ const {
+    keys,
+    defineProperty
+ } = Object ;
+
+ function generatePropertyFn(fn , record , name) {
+     
+    if(isFunction(fn)){
+
+        return fn.bind(record , name) ;
+    }
+
+    return emptyFn ;
+ }
 
  return {
-    read:new Function('data' , codes.join(''))
+    read(data){
+
+        let items = from(get(data , root)),
+            records = [];
+    
+        defineProperty(records , '__ZBEE_PARENT__' , {
+            writable:true
+        }) ;
+    
+        for(let item of items){
+    
+            let record = {},
+                names = keys(properties);
+    
+            defineProperty(record , '__ZBEE_PARENT__' , {
+                value:records,
+                writable:true
+            }) ;
+    
+            for(let name of names){
+    
+                let property = properties[name] ;
+    
+                if(isString(property)){
+    
+                    record[name] = get(item , property) ;
+                
+                }else if(isObject(property)){
+    
+                    let {
+                        mapping,
+                        model,
+                        multi = true,
+                        set,
+                        get
+                    } = property ;
+    
+                    if(mapping){
+    
+                        record[name] = get(item , mapping) ;
+                    
+                    }else if(model){
+    
+                        let result = createReader(model).read(item) ;
+                        
+                        if(multi === false && result.length){
+    
+                            result = result[0] ;
+    
+                        }
+    
+                        result.__ZBEE_PARENT__ = record ;
+    
+                        record[name] = result;
+    
+                    }else if(set || get){
+    
+                        defineProperty(record , name , {
+                            enumerable:true,
+                            set:generatePropertyFn(set , record , name),
+                            get:generatePropertyFn(get , record , name)
+                        }) ;
+                    }
+                }
+            }
+    
+            records.push(record) ;
+        }
+    
+        return records ;
+    }
  } ;
