be4c9044652a1ee6659dee421b36cc7f7d962ccd
陈治文 <cheng022074@qq.com>
2019-10-12 15:45:22 +0800

    是否为数据结构

---
+++ src/is/data/structure.fn.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * 判定指定数据是否为数据结构
+ * 
+ * @import isObject from is.object.simple
+ * 
+ * @import is.array
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @return {boolean} 如果为数据记录则返回 true , 否则返回 false 
+ * 
+ */
+
+ return is(data) && isArray(data) ;
