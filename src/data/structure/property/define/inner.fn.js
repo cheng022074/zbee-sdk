0a2bc0743183695b76b88b998ea4b5cb3fb905ed
陈治文 <cheng022074@qq.com>
2019-10-13 14:22:17 +0800

    在定义内部数据属性时，检查给定数据是否包含有 _ZBEE_DATA_INNER__ 属性，如果有使用该属性进行内部数据属性初始化

--- src/data/structure/property/define/inner.fn.js
+++ src/data/structure/property/define/inner.fn.js
@@ -5,8 +5,23 @@
  * 
  * @param {mixed} structure 数据结构
  * 
+ * @param {object} raw 原始数据
+ * 
  */
 
-Object.defineProperty(structure , '__ZBEE_DATA_INNER__' , {
-    value:{}
+ const NAME = '__ZBEE_DATA_INNER__' ;
+
+ let value ;
+
+ if(raw.hasOwnProperty(NAME)){
+
+    value = raw[NAME] ;
+ 
+ }else{
+
+    value = {} ;
+ }
+
+Object.defineProperty(structure , NAME , {
+    value
 }) ;
