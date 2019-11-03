4318576511b7eaeb46ec3c228a5ca46735a5282b
陈治文 <cheng022074@qq.com>
2019-11-03 19:08:14 +0800

    可显示 、可剪切的圆弧

--- src/canvas/draw/line/arc.fn.js
+++ src/canvas/draw/line/arc.fn.js
@@ -7,6 +7,10 @@
  * 
  * @import degree2radian from math.degree2radian
  * 
+ * @import doBegin from canvas.begin
+ * 
+ * @import doEnd from canvas.end
+ * 
  * @param {canvas.Context} context 画板的上下文对象
  * 
  * @param {object} [config = {}] 画线配置
@@ -23,14 +27,18 @@
  * 
  * @param {number} [config.counterclockwise = false] 如果为 false 则为顺时针，反之为逆时针
  * 
+ * @param {boolean} [config.independent = true] 是否为独立图形
+ * 
+ * @param {boolean} [config.clip = false] 是否为剪切路径
+ * 
  * @param {object} [...config.styles] 画线样式
  * 
  */
 
-context.beginPath();
+doBegin(context , independent) ;
 
 assign(context , styles) ;
 
 context.arc(x , y , r , degree2radian(start) , degree2radian(end) , counterclockwise) ;
 
-context.stroke();
+doEnd(context , clip) ;
