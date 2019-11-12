8381e3d00e4c4ee27e43e36e3343e03d52bb686c
陈治文 <cheng022074@qq.com>
2019-11-12 16:55:52 +0800

    将 绘图 put 变更为 insert

---
+++ src/browser/canvas/data/insert.fn.js
@@ -0,0 +1,20 @@
+
+/**
+ * 
+ * 函数实现说明
+ * 
+ * @import is.number
+ * 
+ * @param {canvas.Context} context 画板的上下文对象
+ * 
+ * @param {object} [config = {}] 绘制配置
+ * 
+ * @param {ImageData} config.data 截图数据
+ * 
+ * @param {number} [config.x = 0] 截图横坐标
+ * 
+ * @param {number} [config.y = 0] 截图纵坐标
+ * 
+ */
+
+context.putImageData(data , x , y) ;
