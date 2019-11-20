9b0109ebcaa0e6a22ddccec4ea82e49af1e2a779
陈治文 <cheng022074@qq.com>
2019-11-20 14:05:03 +0800

    多次返回 Promise

---
+++ src/promise/multi.fn.js
@@ -0,0 +1,52 @@
+
+/**
+ * 
+ * 多次返回的 Promise 封装
+ * 
+ * @param {function} fn 函数回调
+ * 
+ * 
+ */
+
+ function execute(field , data){
+
+    let fns = this[`$${field}`];
+
+    for(let fn of fns){
+
+        data = fn(data) ;
+    }
+ }
+
+ function apply(field , fn){
+
+    let me = this ;
+
+    me[`$${field}`].push(fn) ;
+
+    return me ;
+ }
+
+ class main{
+
+    constructor(fn){
+
+        let me = this ;
+
+        setTimeout(() => fn(data => execute.call(me , 'resolves' , data) , error => execute.call(me , 'rejects' , error)) , 0) ;
+
+        me.$resolves = [] ;
+
+        me.$rejects = [] ;
+    }
+
+    then(resolve){
+
+        return apply.call(this , resolve) ;
+    }
+
+    catch(reject){
+
+        return apply.call(this , reject) ;
+    }
+ }
