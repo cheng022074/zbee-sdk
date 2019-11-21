087bbb3c071195b52e3950a6bcda16a7ba96341e
陈治文 <cheng022074@qq.com>
2019-11-21 10:50:41 +0800

    调试通道

---
+++ debug/data/connection/channel.js
@@ -0,0 +1,40 @@
+
+/**
+ * 
+ * 调试通道
+ * 
+ * @import Channel from data.connection.channel value
+ * 
+ */
+
+const {
+    MessageChannel
+} = require('worker_threads'),
+{
+    port1,
+    port2
+} = new MessageChannel();
+
+let connection = new Channel({
+    receiver:port1,
+    sender:port2
+}) ;
+
+connection.function('add' , ({
+    value1,
+    value2
+}) =>{
+
+    return value1 + value2 ;
+
+}) ;
+
+(async () =>{
+
+    console.log('结果' , await connection.call('add' , {
+        value1:1,
+        value2:5
+    })) ;
+
+})() ;
+
