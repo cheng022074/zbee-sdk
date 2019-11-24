18e2a6852e4d6b6a6fafcde77ad7e02c9e233683
陈治文 <cheng022074@qq.com>
2019-11-24 15:03:31 +0800

    JSON 读取器

--- src/data/reader/json.fn.js
+++ src/data/reader/json.fn.js
@@ -2,106 +2,17 @@
  * 
  * JSON 数据读取器
  * 
- * @import object.value.get
+ * @import Reader from data.reader value
  * 
- * @import is.string
- * 
- * @import is.function
- * 
- * @import array.from
- * 
- * @import is.empty
- * 
- * @import getModel from data.model.get
- * 
- * @param {object} [config = {}] 读取参数设置
- * 
- * @param {string} [config.rootProperty = '.'] 读取数据的根
- * 
- * @param {function} [config.model] 数据模型类
- * 
- * @param {boolean} [config.isModelData = true] 是否解析成模型化的数据 
- * 
- * @return {function} 读取器所生成的解析函数
+ * @import get from object.value.get
  * 
  */
 
- function main({
-   rootProperty,
-   model,
-   isModelData
- }){
+ class main extends Reader{
 
-   const Model = getModel(model) ;
+   getData(data , path){
 
-   let {
-      fields
-   } = Model ;
-
-   return  (new Function('data' , `
-
-      var me = this,
-         include = me.include,
-         converts = me.converts,
-         createModel = me.createModel,
-         get = include('object.value.get'),
-         from = include('array.from'),
-         isEmpty = include('is.empty');
-
-      ${generate_get_root_data(rootProperty)}
-
-      data = from(data) ;
-
-      var result = [],
-          len = data.length;
-
-      for(var i = 0 ; i < len ; i ++){
-
-         var item = {},
-             currentItem = data[i];
-
-         ${generate_get_field_data(fields)}
-
-         result.push(createModel(item)) ;
-      }
-
-      return result;
-
-    `)).bind({
-       include,
-       converts:fields.converts,
-       createModel:data => isModelData ? new Model({
-         innerData:data
-       }) : data
-    });
- }
-
- function generate_get_root_data(rootProperty){
-
-   if(rootProperty !== '.'){
-
-      return `data = get(data , '${rootProperty}');` ;
+      return get(data , path) ;
    }
-
-   return '' ;
- }
-
- function generate_get_field_data({
-   names
- }){
-
-   let result = [] ;
-
-   for(let name of names){
-
-      result.push(`item['${name}'] = converts['${name}'](currentItem);`) ;
-
-      result.push(`if(isEmpty(item['${name}'])){
-
-         delete item['${name}'];
-      
-      }`) ;
-   }
-
-   return result.join('') ;
+   
  }
