103f9f6fecaae4ae6a255d003c8bad3ecc5e8ee8
陈治文 <cheng022074@qq.com>
2019-11-27 12:55:15 +0800

    数据推送器实现

---
+++ src/data/pusher.fn.js
@@ -0,0 +1,81 @@
+
+/**
+ * 
+ * 数据推送器
+ * 
+ * @import is.defined
+ * 
+ * @import clear from array.clear
+ * 
+ * @import defer from function.defer
+ * 
+ * @param {function} onOpen 当开始推送时回调函数
+ * 
+ * @param {function} onClose 当开始推送时回调函数
+ * 
+ */
+
+ class main{
+
+    constructor(onOpen , onClose){
+
+        let me = this ;
+
+        me.onOpen = onOpen ;
+
+        me.onClose = onClose ;
+
+        me.callbacks = [] ;
+
+        defer(open , {
+            scope
+        }) ;
+    }
+
+    close(){
+
+        let {
+            callbacks,
+            onClose
+        } = this ;
+
+        clear(callbacks) ;
+
+        onClose() ;
+    }
+
+    push(callback){
+
+        let me = this,
+            {
+                callbacks
+            } = me;
+
+        callbacks.push(callback) ;
+
+        return me ;
+    }
+ }
+
+ function open(){
+
+    let me = this,
+    {
+        callbacks,
+        onOpen
+    } = me;
+
+    onOpen(data => {
+
+        for(let callback of callbacks){
+
+            let result = callback(data) ;
+
+            if(isDefined(result)){
+
+                data = result ;
+            }
+        }
+
+    }) ;
+ }
