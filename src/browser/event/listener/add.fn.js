9649a7a847a5a60e781ad8798566fd0ece1cfbab
陈治文 <cheng022074@qq.com>
2020-03-18 11:43:41 +0800

    浏览器全局事件添加监听

--- src/browser/event/listener/add.fn.js
+++ src/browser/event/listener/add.fn.js
@@ -13,62 +13,102 @@
  * 
  * @import add from event.listener.add
  * 
+ * @import isObject from is.object.simple
+ * 
  * @param {mixed} target 目标
  * 
- * @param {string} event 目标监听事件
+ * @param {mixed} event 目标监听事件
  * 
- * @param {mixed} fn 目标监听回调
+ * @param {mixed} [fn] 目标监听回调
  * 
  * @param {object} [config = {}] 配置
- * 
- * @param {string} [config.selector] 选择器
- * 
- * @param {boolean} [config.stop = false] 停止冒泡
- * 
- * @param {boolean} [config.prevent = false] 禁用默认浏览器行为
- * 
- * @param {boolean} [config.once = false] 是否只监听一次
  * 
  * @return {mixed} 返回说明 
  * 
  */
 
-if(listeners.has(target , event , fn)){
+ function main(target , event , fn , config){
 
-    return ;
-}
-
-let listener = e =>{
+    if(isObject(event)){
 
         let {
-            target
-        } = e ;
+            scope,
+            ...listeners
+        } = event ;
 
-        if(stop){
+        let names = Object.keys(listeners) ;
+
+        for(let name of names){
+
+            if(isObject(listener)){
+
+                let {
+                    fn,
+                    ...options
+                } = listener ;
+    
+                options.scope = options.scope || scope ;
+    
+                add(target , name , fn , options) ;
             
-            stopEvent(e) ;
+            }else{
+    
+                add(target , name , listeners[name] , {
+                    scope
+                }) ;
+            }
         }
+        
+    }else{
 
-        if(prevent){
+        add(target , event , fn , config) ;
+    }
 
-            preventEvent(e) ;
-        }
+ }
 
-        if(selector){
+ function add(target , event , fn , {
+     selector,
+     stop = false,
+     prevent = false,
+     ...config
+ }){
 
-            if(is(target , selector)){
+    if(listeners.has(target , event , fn)){
 
+        return ;
+    }
+    
+    let listener = e =>{
+    
+            let {
+                target
+            } = e ;
+    
+            if(stop){
+                
+                stopEvent(e) ;
+            }
+    
+            if(prevent){
+    
+                preventEvent(e) ;
+            }
+    
+            if(selector){
+    
+                if(is(target , selector)){
+    
+                    fn(e) ;
+                }
+                
+            }else{
+    
                 fn(e) ;
             }
-            
-        }else{
+        };
+    
+    listeners.set(target , event , fn , listener) ;
+    
+    add(target , event , listener , config) ;
+ }
 
-            fn(e) ;
-        }
-    };
-
-listeners.set(target , event , fn , listener) ;
-
-add(target , event , listener , {
-    once
-}) ;
