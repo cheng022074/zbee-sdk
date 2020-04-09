c0a5b1597411eb248a3068e90ecd17214a4d3dd4
陈治文 <cheng022074@qq.com>
2020-04-09 14:13:36 +0800

    初始化数据缓存参数设计

---
+++ src/data/cache/constructor.fn.js
@@ -0,0 +1,16 @@
+
+/**
+ * 
+ * 初始化数据缓存
+ * 
+ * @param {object} [options = {}] 配置
+ * 
+ * @param {boolean} [options.proxy = true] 是否启用 Proxy 模式，默认启用
+ * 
+ * @param {string} options.schema 数据缓存结构名称
+ * 
+ * @param {string} options.name 数据缓存名称
+ * 
+ */
+
+ // 代码实现
