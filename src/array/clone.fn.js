95edc075655184d4ac102222dae7da65b1414e72
陈治文 <cheng022074@qq.com>
2020-04-10 14:51:41 +0800

    数组拷贝

--- src/array/clone.fn.js
+++ src/array/clone.fn.js
@@ -3,9 +3,41 @@
  * 
  * 对于数组进行拷贝
  * 
+ * @import is.array
+ * 
+ * @import isObject from is.objec.simple
+ * 
+ * @import is.date
+ * 
+ * @import array.clone
+ * 
+ * @import assign from object.assign
+ * 
+ * @import date.clone
+ * 
  * @param {array} data 数组
  * 
  * 
  */
 
- return data.slice(0) ;
+ let result = [] ;
+
+ for(let item of data){
+
+    if(isObject(item)){
+
+        item = assign({} , item) ;
+
+    }else if(isArray(item)){
+
+        item = arrayClone(item) ;
+    
+    }else if(isDate(item)){
+
+        item = dateClone(item) ;
+    }
+
+    result.push(item) ;
+ }
+
+ return result ;
