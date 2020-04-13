db6ac16fbad676e7700641014793c7959ce83ddf
陈治文 <cheng022074@qq.com>
2020-04-13 13:43:43 +0800

    清理缓存

--- src/data/cache/clear.fn.js
+++ src/data/cache/clear.fn.js
@@ -3,10 +3,58 @@
  * 
  * 清空缓存
  * 
+ * @import add from event.listener.add
+ * 
+ * @import clear from .clear scoped
+ * 
  */
 
- let {
-    proxy
- } = this ;
+ function main(){
 
- proxy.call('clearCache') ;
+   let me = this,
+   {
+      proxy,
+      readyState
+   } = me ;
+  
+   switch(readyState){
+  
+     case -1:
+     case 1:
+
+        me.readyState = 2 ;
+
+        me.fireEvent('unloading') ;
+
+        doClear.call(me , proxy.call('clearCache')) ;
+  
+        break ;
+  
+     case 0:
+  
+        add(me , 'load' , () => clear() , {
+           once:true
+        }) ;
+   }
+ }
+
+ function doClear(result){
+
+   let me = this ;
+
+   if(isPromise(result)){
+
+      result.then(doClear) ;
+   
+   }else{
+
+      me.readyState = -1 ;
+
+      delete me.data ;
+
+      me.fireEvent('unload') ;
+   }
+
+ }
+
+ 
