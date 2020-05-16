6f1a1cd5cf1bf5abaec42cf4469276da4b706bad
cheng022074 <cheng022074@qq.com>
2020-05-16 19:25:51 +0800

    设置属性

--- src/object/property/define.fn.js
+++ src/object/property/define.fn.js
@@ -6,6 +6,8 @@
  * 
  * @import innerName from .inner.name
  * 
+ * @import set from .define.set
+ * 
  * @param {object} target 目标对象
  * 
  * @param {string} name 属性名称
@@ -15,6 +17,10 @@
  * @param {boolean} [options.mode = 'readonly'] 读写模式
  * 
  * @param {string} [options.value] 属性初始始化值
+ * 
+ * @param {boolean} [options.equals] 判断属性值是否相等
+ * 
+ * @param {boolean} [options.isEnablePropertyChangeEvent = true] 是否抛出属性改变事件，如果属性改变的话
  * 
  */
 
@@ -32,10 +38,7 @@ switch(mode){
     case 'writeonly':
 
         Object.defineProperty(target , name , {
-            set(value){
-
-                this[innerName(name)] = value ;
-            }
+            set:set(name , isEnablePropertyChangeEvent , equals)
         }) ;
 
         innerDefine(target , name , value) ;
@@ -46,10 +49,7 @@ switch(mode){
 
         Object.defineProperty(target , name , {
             [name]:{
-                set(value){
-
-                    this[innerName(name)] = value ;
-                },
+                set:set(name , isFirePropertyChangeEvent , equals),
 
                 get(){
 
---
+++ src/object/property/define/set.fn.js
@@ -0,0 +1,38 @@
+
+/**
+ * 
+ * 设置属性
+ * 
+ * @import innerName from ..inner.name
+ * 
+ * @import is.function
+ * 
+ * @param {string} name 属性名称
+ * 
+ * @param {boolean} isEnablePropertyChangeEvent 是否启动改变属性事件
+ * 
+ * @param {function} [isEquals] 属性值判断是否相等，只在启动改变属性事件有效
+ * 
+ */
+
+ if(isEnablePropertyChangeEvent){
+
+    return value => {
+
+        let me = this,
+            target = innerName('EVENT_TARGET');
+
+        if(target && isFunction(target.fireEvent)){
+
+            let oldValue = me[innerName(name)] ;
+
+            if(!isEquals(value , oldValue)){
+
+                target.fireEvent('propertychange' , name , value , oldValue) ;
+            }
+
+        }
+    } ;
+ }
+
+ return value => this[innerName(name)] = value ;
