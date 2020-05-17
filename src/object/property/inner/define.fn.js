e564208db50fbba2e04297dd6371721f502e3674
cheng022074 <cheng022074@qq.com>
2020-05-17 17:11:12 +0800

    属性定义

--- src/object/property/inner/define.fn.js
+++ src/object/property/inner/define.fn.js
@@ -5,15 +5,36 @@
  * 
  * @import innerName from .name
  * 
+ * @import isObject from is.object.simple
+ * 
  * @param {object} target  定义内部属性的宿主
  * 
- * @param {string} name 内部属性名称
+ * @param {string|object} name 内部属性名称
  * 
  * @param {mixed} value 内部属性值
  * 
  */
 
- Object.defineProperty(target , innerName(name) , {
-     value,
-     writable:true
- }) ;
+ if(isObject(name)){
+
+    let properties = name,
+        names = Object.keys(properties),
+        innerProperties = {};
+
+    for(let name of names){
+
+        innerProperties[innerName(name)] = {
+            value:properties[name],
+            writable:true
+        }
+    }
+
+    Object.defineProperties(target , innerProperties) ;
+ 
+ }else{
+
+    Object.defineProperty(target , innerName(name) , {
+        value,
+        writable:true
+    }) ;
+ }
