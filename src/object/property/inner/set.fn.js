a4a2bf061608afd2b5673cd4d85d1a267c731f0d
cheng022074 <cheng022074@qq.com>
2020-05-17 09:28:45 +0800

    设置内部属性值

---
+++ src/object/property/inner/set.fn.js
@@ -0,0 +1,18 @@
+
+/**
+ * 
+ * 设置内部属性的值
+ * 
+ * @import innerName from .name
+ * 
+ * @param {object} target  定义内部属性的宿主
+ * 
+ * @param {string} name 内部属性名称
+ * 
+ * @param {mixed} value 内部属性值
+ * 
+ */
+
+return this[innerName(name)] = value;
+
+ 
