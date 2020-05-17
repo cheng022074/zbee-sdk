c1c5a0871dca186c3f456cc5cd104f0db590c7c4
cheng022074 <cheng022074@qq.com>
2020-05-17 20:45:15 +0800

    去除内部属性

---
+++ src/object/property/inner/remove.fn.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * 去除内部属性
+ * 
+ * @import innerName from .name
+ * 
+ * @param {object} target  定义内部属性的宿主
+ * 
+ * @param {string} name 内部属性名称
+ * 
+ */
+
+ delete this[innerName(name)] ;
