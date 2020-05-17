bb0fde9da56ca61afd4a13cc36ff56ec81087aa3
cheng022074 <cheng022074@qq.com>
2020-05-17 20:45:45 +0800

    重写数据记录集合

--- src/data/recordset.fn.js
+++ src/data/recordset.fn.js
@@ -1,335 +1,63 @@
+
 /**
  * 
- * 数据记录集合
+ * 创建一个专有于数据读取器的数组
  * 
- * @import remove from array.remove
+ * @import define from object.property.inner.define
  * 
- * @import removeIndex from array.remove.index
+ * @import get from object.property.inner.get
  * 
- * @import insert from array.insert
+ * @import createObservable from .observable
  * 
- * @import from from array.from
+ * @import is from is.data.record
  * 
- * @import isModel from is.data.model
+ * @param {data.Reader} reader 数据读取器
  * 
- * @import isObject from is.object.simple
+ * @param {array} records 数据记录数组
  * 
- * @import is.defined
- * 
- * @import is.string
- * 
- * @import is.function
- * 
- * @import clone from array.clone
- * 
- * @param {data.Store} store 存储器
+ * @return {array} 数组
  * 
  */
 
- class main{
+const {
+    push
+} = Array.prototype ;
 
-    constructor(store){
+class main extends Array{
+
+    constructor(reader , records){
+
+        super() ;
 
         let me = this ;
 
-        me.recordMap = new Map() ;
+        define(me , 'reader' , reader) ;
 
-        me.recordData = [] ;
-
-        me.store = store ;
-    }
-
-    /**
-     * 
-     * 转换成数组输出
-     * 
-     * @return {array}
-     * 
-     */
-    toArray(){
-
-        return clone(this.recordData) ;
-    }
-
-    /**
-     * 
-     * 根据编号获得数据记录
-     * 
-     * @param {mixed} id 数据记录编号
-     * 
-     * @return {data.Model} 
-     * 
-     */
-    getById(id){
-
-        let {
-            recordMap
-        } = this ;
-
-        return recordMap.get(id) ;
-    }
-
-    /**
-     * 
-     * 根据数据记录获得在存储器中的位置
-     * 
-     * @param {data.Model} record 数据记录
-     * 
-     * @return {number} 数据记录位置
-     * 
-     */
-    indexOf(record){
-
-        let {
-            recordData
-        } = this ;
-
-        return recordData.indexOf(record) ;
-    }
-
-    /**
-     * 
-     * 查寻数据记录
-     * 
-     * @param {mixed} property 属性名称
-     *  
-     * @param {mixed} [value] 属性值
-     * 
-     * @return {array} 数据记录集合
-     *  
-     */
-    findRecords(property , value){
-
-        let {
-            recordData:records
-        } = this ;
-
-        if(isString(property)){
-
-            let result = [] ;
-
-            for(let record of records){
-
-                if(record.get(property) === value){
-
-                    result.push(record) ;
-                }
-            }
-
-            return result ;
-
-        }else if(isFunction(property)){
-
-            let result = [] ;
-
-            for(let record of records){
-
-                if(property.call(value , record) === true){
-
-                    result.push(record) ;
-                }
-            }
-
-            return result ;
-
-        }
-    }
-
-    /**
-     * 
-     * 清理
-     * 
-     */
-    clear(){
-
-        let {
-            recordMap,
-            recordData
-        } = this ;
-
-        recordMap.clear() ;
-
-        recordData.length = 0 ;
-    }
-
-    /**
-     * 
-     * @param {function} fn 循环执行的函数
-     *  
-     * @param {mixed} scope 
-     */
-    each(fn , scope){
-
-        let {
-            recordData
-        } = this ;
-
-        for(let record of recordData){
-
-            fn.call(scope , record) ;
-        }
-    }
-
-    /**
-     * 
-     * 添加数据记录
-     * 
-     * @param {mixed} records 数据记录
-     * 
-     * 
-     */
-    add(records){
-
-        let me = this,
-        {
-            recordData
-        } = me ;
-
-        records = getRecords.call(me , records) ;
-
-        recordData.push(...records) ;
-
-        resetRecordData(recordData) ;
-
-        return records ;
-    }
-     /**
-     * 
-     * 插入数据记录
-     * 
-     * @param {mixed} records 数据记录
-     * 
-     * 
-     */
-    insert(index , records){
-
-        let me = this,
-        {
-            recordData
-        } = me ;
-
-        records = getRecords.call(me , records) ;
-
-        insert(recordData , index , ...records) ;
-
-        resetRecordData(recordData) ;
-
-        return records ;
-    }
-     /**
-     * 
-     * 删除数据记录
-     * 
-     * @param {mixed} records 数据记录
-     * 
-     * 
-     */
-    remove(records){
-
-        records = from(records) ;
-
-        let {
-            recordMap,
-            recordData
-        } = this,
-        removeRecords = [];
+        define(me , 'observable' , createObservable()) ;
 
         for(let record of records){
 
-            let {
-                id
-            } = record ;
+            if(is(record)){
 
-            if(recordMap.has(id)){
+                get(record , 'observable').belongTo(me) ;
 
-                recordMap.delete(id) ;
-
-                remove(recordData , record) ;
-
-                record.unbindStore() ;
-
-                removeRecords.push(record) ;
+                push.call(me , record) ;
             }
         }
-
-        return removeRecords ;
-    }
- }
-
- /**
-  * 
-  * 待处理的数据记录
-  * 
-  * @param {mixed} records 数据记录
-  * 
-  */
- function getRecords(records){
-
-    records = from(records) ;
-
-    let {
-        recordMap,
-        recordData,
-        store
-    } = this,
-    result = [];
-
-    for(let record of records){
-
-        if(isObject(record)){
-
-            record = store.createRecord(record) ;
-        }
-
-        if(isModel(record)){
-
-            let {
-                id
-            } = record ;
-
-            if(recordMap.has(id)){
-
-                record = recordMap.get(id) ;
-    
-                removeRecord(recordData , record) ;
-
-                record.set(record.data) ;
-            
-            }else{
-
-                recordMap.set(id , record) ;
-
-                if(record.isBindStore && record.store !== store){
-
-                    continue ;
-                }
-
-                record.bindStore(store) ;
-            }
-
-            result.push(record) ;
-        }
     }
 
-    return result ;
- }
+    push(...raws){  
 
- function removeRecord(recordData , record){
-
-    let index = recordData.indexOf(record) ;
-
-    if(index !== -1){
-
-        recordData[index] = undefined ;
+        super.push(...get(this , 'reader').read(raws)) ;
     }
- }
 
- function resetRecordData(recordData){
+    unshift(...raws){
 
-    let index ;
+        super.unshift(...get(this , 'reader').read(raws)) ;
+    }
 
-    while((index = recordData.indexOf(undefined)) !== -1){
+    splice(index , howMany , ...raws){
 
-        removeIndex(recordData , index) ;
+        super.splice(index , howMany , ...get(this , 'reader').reader(raws)) ;
     }
  }
