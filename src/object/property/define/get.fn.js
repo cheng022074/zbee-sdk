7453f6190a60b6061b4bacfca3bf3f82dfaadde0
cheng022074 <cheng022074@qq.com>
2020-05-17 09:31:10 +0800

    应用获取属性值

---
+++ src/object/property/define/get.fn.js
@@ -0,0 +1,27 @@
+
+/**
+ * 
+ * 获取属性
+ * 
+ * @import is.function
+ * 
+ * @import get from ..inner.get
+ * 
+ * @param {string} name 属性名称
+ * 
+ * @param {function} onGet 获取属性值
+ * 
+ */
+ 
+return () => {
+
+   let me = this,
+       value = get(me , name) ;
+
+   if(isFunction(onGet)){
+
+       return onGet.call(me , value) ;
+   }
+
+   return value ;
+} ;
