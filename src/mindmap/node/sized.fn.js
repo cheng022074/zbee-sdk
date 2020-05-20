5c0eacbb2939cfe03efb743253742216891f1464
陈治文 <cheng022074@qq.com>
2020-05-20 13:15:35 +0800

    指定节点是否同步了尺寸

---
+++ src/mindmap/node/sized.fn.js
@@ -0,0 +1,19 @@
+
+/**
+ * 
+ * 指定节点是否同步获取了宽度
+ * 
+ * @param {data.Record} node 节点
+ * 
+ * @return {boolean} 如果已获取尺寸则返回 true , 否则返回 false 
+ * 
+ */
+
+ let {
+    waitInitSizeNodes
+ } = this,
+ {
+    id
+ } = node;
+
+ return waitInitSizeNodes.has(id) ;
