44955dafd58cfcb60d9d81c053efae2e3b7e451d
cheng022074 <cheng022074@qq.com>
2020-05-26 20:59:50 +0800

    恢复原先事件派发方式

--- src/mindmap/fire/draw.fn.js
+++ src/mindmap/fire/draw.fn.js
@@ -7,8 +7,6 @@
  * 
  * @import data from ..data scoped
  * 
- * @import defer from function.defer
- * 
  * @param {mixed} data 参数说明
  * 
  * @return {mixed} 返回说明 
@@ -17,30 +15,17 @@
 
  let me = this,
  {
-   fireDrawEventTimerId
- } = me;
+    visibilityNodes
+ } = me,
+ {
+     nodes,
+     selectedNode,
+     lines
+ } = data(visibilityNodes.values() , true);
 
- if(fireDrawEventTimerId){
-
-   clearTimeout(fireDrawEventTimerId) ;
- }
-
- me.fireDrawEventTimerId = defer(() => {
-
-   let {
-      visibilityNodes
-   } = me,
-   {
-       nodes,
-       selectedNode,
-       lines
-   } = data(visibilityNodes.values() , true);
-  
-   me.fireEvent('draw' , {
-      nodes,
-      lines,
-      selectedNode,
-      canvas:region()
-   }) ;
-
+ me.fireEvent('draw' , {
+    nodes,
+    lines,
+    selectedNode,
+    canvas:region()
  }) ;
