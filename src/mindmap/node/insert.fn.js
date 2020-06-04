f712bc294772429f71eaa28ed5c11b9d25e1180b
陈治文 <cheng022074@qq.com>
2020-06-04 11:13:12 +0800

    插入问题修复

--- src/mindmap/node/insert.fn.js
+++ src/mindmap/node/insert.fn.js
@@ -29,8 +29,11 @@ if(!isRootNode(baseNode)){
     } = parentNode,
     {
         length
-    } = children,
-    index = children.indexOf(baseNode) + offset ;
+    } = children;
+
+    insertNode = create(insertNode , parentNode) ;
+
+    let index = children.indexOf(baseNode) + offset ;
 
     if(index > length - 1){
 
@@ -40,20 +43,15 @@ if(!isRootNode(baseNode)){
 
         index = 0 ;
     }
-
+    
     if(children[index] === insertNode){
 
         return false ;
     }
 
-    insertNode = create(insertNode , parentNode) ;
-
     children.splice(index , 0 , insertNode) ;
 
-    if(!baseNode.hidden){
-
-        show(insertNode) ;
-    }
+    show(insertNode) ;
 
     return insertNode ;
 }
