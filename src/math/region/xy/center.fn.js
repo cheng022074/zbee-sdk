b13ee562d266ad4beb875ce96cea4ff3e45acd90
陈治文 <cheng022074@qq.com>
2020-07-01 11:38:49 +0800

    设置或者获取方位中心坐标

--- src/math/region/xy/center.fn.js
+++ src/math/region/xy/center.fn.js
@@ -3,21 +3,35 @@
  * 
  * 计算中心点坐标
  * 
- * @import getTop from ..top
+ * @param {object} region 方位信息
  * 
- * @import getBottom from ..bottom
- * 
- * @import getLeft from ..left
- * 
- * @import getRight from ..right
- * 
- * @param {object} config 配置
+ * @param {object} [xy] 需要设置的中心坐标
  * 
  * @return {object} 坐标 
  * 
  */
 
- return {
-     x:getLeft(config) + (getRight(config) - getLeft(config)) / 2,
-     y:getTop(config) + (getBottom(config) - getTop(config)) / 2
- } ;
+let {
+    left,
+    right,
+    top,
+    bottom
+} = region ;
+
+ if(!xy){
+
+    return {
+        x:left + (right - left) / 2,
+        y:top + (bottom - top) / 2
+    } ;
+
+ }
+
+ let {
+    x,
+    y
+} = xy ;
+
+region.left = x - (right - left) / 2 ;
+
+region.top = y - (bottom - top) / 2 ;
