b3f84dc33d4d8f571b5e8efa9db2dc4a193f54fd
陈治文 <cheng022074@qq.com>
2020-07-03 10:22:14 +0800

    检测元素是否拥有样式类

---
+++ src/browser/element/class/has.fn.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * 判断指定元素是否拥有指定样式类
+ * 
+ * @param {HTMLElement} el 元素
+ * 
+ * @param {string} cls 样式类
+ * 
+ * @return {boolean} 如果元素拥有对应样式类则返回 true , 否则返回 false 
+ * 
+ */
+
+ return el.classList.has(cls) ;
