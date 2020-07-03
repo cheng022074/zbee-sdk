362b204ec254b60ff8ceb26fa53075b79ac17337
陈治文 <cheng022074@qq.com>
2020-07-03 11:02:35 +0800

    主动取消忽略

---
+++ src/mindmap/node/ellipsis/cancel/active.fn.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 主动取消省略
+ * 
+ * @import getParentNode from ....parent scoped
+ * 
+ * @import getDeepestLeafNodes from ....nodes.leaf.deepest scoped
+ * 
+ * @imoprt cancelEllipsis from .passive scoped
+ * 
+ */
+
+let {
+    ellipsisRootNode
+ } = this ;
+
+ if(ellipsisRootNode){
+
+    let deepestLeafNodes = getDeepestLeafNodes(ellipsisRootNode);
+
+    for(let deepestLeafNode of deepestLeafNodes){
+
+        deepestLeafNode.hidden = false ;
+
+        getParentNode(deepestLeafNode).expanded = false ;
+    }
+
+    cancelEllipsis() ;
+ }
