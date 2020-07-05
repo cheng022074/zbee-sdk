76ffbacbc263e566e4146a479893969a561e6469
陈治文 <cheng022074@qq.com>
2020-07-05 17:43:17 +0800

    增加针对于游走节点时忽略节点时的处理

--- src/mindmap/select.fn.js
+++ src/mindmap/select.fn.js
@@ -7,6 +7,8 @@
  * 
  * @import data from .node.data scoped
  * 
+ * @import cancelEllipsis from .node.ellipsis.cancel.active scoped
+ * 
  * @param {string} id 节点编号
  * 
  * @param {boolean} [isFireDrawEvent = true] 是否派发重绘事件 
@@ -17,16 +19,21 @@
  {
     visibilityNodes,
     selectedNode,
-    restructuring
+    restructuring,
+    ellipsisRootNode
  } = me ;
 
- if(!restructuring && selectedNode.id !== id && visibilityNodes.has(id)){
+ if(ellipsisRootNode && ellipsisRootNode.id === id){
+
+  cancelEllipsis() ;
+    
+ }else if(!restructuring && selectedNode.id !== id && visibilityNodes.has(id)){
 
    let node = visibilityNodes.get(id) ;
 
    node.selected = true ;
         
-    me.fireEvent('nodeselect' , data(node)) ;
+   me.fireEvent('nodeselect' , data(node)) ;
 
     if(isFireDrawEvent){
 
