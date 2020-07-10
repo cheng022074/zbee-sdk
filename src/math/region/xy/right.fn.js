d8a9f0f304c3188d443774a49f5f1ff1534a2877
陈治文 <cheng022074@qq.com>
2020-07-10 13:50:40 +0800

    去除无效方位计算函数

--- src/math/region/xy/right.fn.js
+++ src/math/region/xy/right.fn.js
@@ -3,14 +3,6 @@
  * 
  * 计算右边坐标
  * 
- * @import getTop from ..top
- * 
- * @import getBottom from ..bottom
- * 
- * @import getLeft from ..left
- * 
- * @import getRight from ..right
- * 
  * @param {object} region 配置
  * 
  * @return {object} 坐标 
--- src/mindmap/region/selected.fn.js
+++ src/mindmap/region/selected.fn.js
@@ -3,22 +3,11 @@
  * 
  * 获得选中节点的方位信息
  * 
- * @import bottom from math.region.bottom
- * 
- * @import right from math.region.right
- * 
- * @import createRegion from math.region
- * 
- * @import getData from ..node.data scoped
+ * @import from from math.region.from
  * 
  * @return {math.Region} 区域 
  * 
  */
 
- let {
-    selectedNode
- } = this,
- region = getData(selectedNode) ;
-
- return createRegion(region.y , right(region) , bottom(region) , region.x) ;
+ return from(this.selectedNode) ;
 
