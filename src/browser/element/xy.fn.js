2755a8e4895f7536670902281ec7bdfaddbf344f
陈治文 <cheng022074@qq.com>
2020-07-21 11:33:21 +0800

    获取元素的坐标

---
+++ src/browser/element/xy.fn.js
@@ -0,0 +1,37 @@
+
+/**
+ * 
+ * 获得元素的坐标
+ * 
+ * @param {HTMLElement} el 元素
+ * 
+ * @return {object} 坐标信息
+ * 
+ */
+
+ const {
+    round
+ } = Math ;
+
+let x = 0,
+    y = 0;
+
+if(el !== document && el !== document.body){
+
+    let {
+        left:bodyLeft,
+        top:bodyTop
+    } = body.getBoundingClientRect(),
+    {
+        left,
+        top
+    } = dom.getBoundingClientRect();
+
+    x = left - bodyLeft,
+    y = top - bodyTop;
+}
+
+return {
+    x:round(x),
+    y:round(y)
+} ;
