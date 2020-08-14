7a758a1fb022c2ca2b8c8759363b5c5777ee642c
cheng022074 <cheng022074@qq.com>
2020-08-14 21:52:13 +0800

    路径识别重构

--- debug/expression/parse/mindmap/price.js
+++ debug/expression/parse/mindmap/price.js
@@ -13,7 +13,7 @@
  * 
  * @import expression.parser.mindmap.nodes.descendant
  * 
- * @import expression.parser.mindmap.path.split
+ * @import expression.parser.mindmap.nodes.children
  * 
  * @import expression.parser.empty
  * 
@@ -29,9 +29,9 @@ let parsers = [
     'expression.parser.mindmap.nodes.descendant',
     'expression.parser.mindmap.node.attribute',
     'expression.parser.mindmap.node.plugin',
-    'expression.parser.mindmap.path.split',
+    'expression.parser.mindmap.nodes.children',
     'expression.parser.number',
     'expression.parser.empty'
  ] ;
 
- console.log(JSON.stringify(parse('-30000' , parsers) , null , 2)) ;
+ console.log(JSON.stringify(parse('sum(.//price/@price)' , parsers) , null , 2)) ;
--- src/expression/parser/mindmap/nodes/children.fn.js
+++ src/expression/parser/mindmap/nodes/children.fn.js
@@ -3,13 +3,13 @@
  * 
  * 解析路径中的所有子节点标识符
  * 
- * @import parse from ..regex
+ * @import parse from ....regex
  * 
  * @param {string} expression 表达式
  * 
  */
 
- return parse(expression , /\*/ , () => ({
+ return parse(expression , /\// , () => ({
     syntax:'nodes',
     name:'children'
 })) ;
