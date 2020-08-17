1705b7b8461bd4a8b05661fe9d0bcf4bb7f40710
陈治文 <cheng022074@qq.com>
2020-08-17 12:52:36 +0800

    加法运算符

---
+++ debug/expression/complie/operator.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 基于字面量的表达式编译
+ * 
+ * @import complie from expression.complie
+ * 
+ * @import expression.parser.number
+ * 
+ * @import expression.parser.string
+ * 
+ * @import expression.parser.boolean
+ * 
+ * @import expression.parser.operator
+ * 
+ * @import expression.complier.literal
+ * 
+ * @import expression.complier.expression
+ * 
+ * @import expression.complier.operator
+ * 
+ */
+
+const 
+context = {},
+parsers = [
+   'expression.parser.string',
+   'expression.parser.number',
+   'expression.parser.boolean',
+   'expression.parser.operator'
+],
+compliers = {
+   literal:'expression.complier.literal',
+   expression:'expression.complier.expression',
+   operator:'expression.complier.operator'
+};
+
+console.log('数字应用加法' , complie(context , '40 + 20' , parsers , compliers)()) ;
+
+console.log('字符串应用加法' , complie(context , '"运算先锋" + "是拥有" + 16 + "年经验的工程师"' , parsers , compliers)()) ;
