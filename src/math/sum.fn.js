17b859790850cb5a78802545a1a8ac4e28e66ad7
陈治文 <cheng022074@qq.com>
2020-08-17 15:33:04 +0800

    参数中可以含有数组项

--- src/math/sum.fn.js
+++ src/math/sum.fn.js
@@ -3,6 +3,10 @@
  * 
  * 求和
  * 
+ * @import is.array
+ * 
+ * @import sum from .sum
+ * 
  * @param {array} [...values] 一组待求和的值
  * 
  * @return {number} 值
@@ -13,7 +17,14 @@
 
  for(let value of values){
 
-    result += value ;
+   if(isArray(value)){
+
+      result += sum(...value) ;
+
+   }else{
+
+      result += value ;
+   }    
  }
 
  return result ;
