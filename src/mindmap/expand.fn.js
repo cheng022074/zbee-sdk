088f1a0db414e91c075225eeebd3ac7d660bf67a
陈治文 <cheng022074@qq.com>
2020-09-04 14:39:13 +0800

    展开与布局脱钩

--- src/mindmap/expand.fn.js
+++ src/mindmap/expand.fn.js
@@ -7,15 +7,17 @@
  * 
  * @import expand from .node.expand scoped
  * 
- * @import tryLayout from .layout.try scoped
- * 
  * @param {string} id 节点编号
  * 
  */
 
 let node = get(id) ;
 
-if(node && expand(node)){
+if(node){
 
-    await tryLayout() ;
+    expand(node) ;
+
+    return true ;
 }
+
+return false ;
