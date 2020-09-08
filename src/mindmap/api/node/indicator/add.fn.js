9d4017884ce720eca86d6bbe1f3163abff21cd4d
陈治文 <cheng022074@qq.com>
2020-09-08 14:19:04 +0800

    API - 添加一个指示器

--- src/mindmap/api/node/indicator/add.fn.js
+++ src/mindmap/api/node/indicator/add.fn.js
@@ -3,10 +3,33 @@
  * 
  * 添加脑图节点指示器
  * 
+ * @import from from ......data.node.from scoped
+ * 
+ * @import has from .has scoped
+ * 
  * @param {mixed} node 脑图节点
  * 
- * @param {mixed} components 组件设置
+ * @param {string} id 组件相对于脑图节点的唯一标识
+ * 
+ * @param {mixed} config 组件设置
  * 
  */
 
- console.log('添加脑图节点指示器') ;
+let  {
+    indicators
+} = from(node) ;
+
+if(!has(node , id)){
+
+    indicators.push({
+        id,
+        ...config
+    }) ;
+
+    this.layout() ;
+}
+
+
+
+
+
