92aa64568b06a015cdf505a88aac67035840b867
陈治文 <cheng022074@qq.com>
2020-09-09 18:07:59 +0800

    设置背景颜色

---
+++ src/mindmap/api/node/color/background.fn.js
@@ -0,0 +1,23 @@
+
+/**
+ * 
+ * 设置背景颜色
+ * 
+ * @import is.defined
+ * 
+ * @import info from ....data.node.info scoped
+ * 
+ * @import from from ....data.node.from scoped
+ * 
+ * @param {mixed} [node] 脑图节点
+ * 
+ * @param {string} backgroundColor 背景颜色
+ * 
+ * @return {mixed} 属性值
+ * 
+ */
+
+info(node , {
+    backgroundColor
+} , false) ; 
+ 
