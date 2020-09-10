d46965243d846eb9b30751e2e31e5a8a6a2d24d3
陈治文 <cheng022074@qq.com>
2020-09-10 15:25:34 +0800

    AJAX 数据连接

--- src/data/channel/client.fn.js
+++ src/data/channel/client.fn.js
@@ -3,124 +3,59 @@
  * 
  * 数据通道客户端基本实现
  * 
- * @import get from object.property.inner.get
- * 
- * @import define from object.property.inner.define
- * 
- * @import add from event.listener.add
- * 
- * @import remove from event.listener.remove
- * 
- * @import is.string
- * 
- * @import Channel from ..channel value
+ * @import Observable from mixin.observable
  * 
  * @class
  * 
  */
 
-class main extends Channel{
-
-    constructor(target){
-
-        super(target) ;
-
-        let me = this,
-        {
-            __ZBEE_CLASS_NAME__
-        } = me;
-
-        if(__ZBEE_CLASS_NAME__ === 'src::data.channel.client'){
-
-            add(me , {
-                data:'onData',
-                dataerror:'onErrorData',
-                scope:me
-            }) ;
-
-            define(me , 'defaultEventName' , `${__ZBEE_CLASS_NAME__}-${Date.now()}`) ;
-        }
-    }
+ class main extends mixins({
+    mixins:[
+        Observable
+    ]
+}){
 
     getEventNameByParams(params){
 
-        let target = get(this , 'target') ;
-
-        if(target){
-
-            return target.getEventNameByParams(params) ;
-        }
+        return JSON.stringify(params) ;
     }
 
-    onErrorData(client , data , params){
+    processSendParams(params){
 
-        fireEvent.call(this , data , params , 'error') ;
+        return params ;
     }
 
-    onData(client , data , params){
-
-        fireEvent.call(this , data , params) ;
-    }
-
-    async send(params , isReturnData = false){
+    send(params , isReturnData = false){
 
         let me = this,
-            target = get(me , 'target'),
-            data;
+            eventName = me.getEventNameByParams(params),
+            dataEvent = eventName,
+            errorEvent = `${eventName}-error`,
+            fireDataEvent = (...params) => me.fireEvent(dataEvent , ...params),
+            fireErrorEvent = (...params) => me.fireEvent(errorEvent , ...params);
+
+        params = me.processSendParams(params) ;
 
         if(isReturnData){
 
-            data = new Promise(callback => {
+            return new Promise((resolve , reject) => {
 
-                let event = getEventName.call(me , params),
-                    listener = (client , data , currentParams) => {
+                me.on({
+                    [dataEvent]:resolve,
+                    [errorEvent]:reject
+                }) ;
 
-                        if(params === currentParams){
-
-                            remove(me , event , listener) ;
-
-                            callback(data) ;
-    
-                        }
-
-                    } ;
-
-                add(me , event , listener) ;
+                me.doSend(params , fireDataEvent , fireErrorEvent) ;
 
             }) ;
-            
         }
 
-        let state = await target.call('send' , params) ;
+        me.doSend(params , fireDataEvent , fireErrorEvent) ;
+    }
 
-        if(isReturnData){
+    doSend(){
 
-            return data ;
-        }
-
-        return state ;
 
     }
 
-    async cancel(params){
-
-        let target = get(this , 'target') ;
-
-        return await target.call('cancel' , params) ;
-    }
- }
-
- function getEventName(params){
-
-    let me = this ;
-
-    return get(me , 'target').callIf('getEventNameByParams' , params) || get(me , 'defaultEventName');
-
- }
-
- function fireEvent(data , params , eventSuffix = ''){
-
-    let me = this ;
-
-    me.fireEvent(`${getEventName.call(me , params)}${eventSuffix}` , data , params) ;
  }
--- src/data/channel/client/axios.fn.js
+++ src/data/channel/client/axios.fn.js
@@ -13,31 +13,19 @@ const axios = require('axios');
 
 class main extends Channel{
 
-    send(params){
-
-        let me = this ;
-
-        return axios(me.processSendParams(params)).then(({
-            data
-        }) => {
-
-            let {
-                op,
-                data:result
-            } = data ;
-
-            if(op.code === 'Y'){
-
-                me.fireEvent('data' , result , params) ;
-            
-            }else{
-
-                me.fireEvent('dataerror' , op , params) ;
-            }
-
-            return true ;
-
-        }) ;
+    doSend(params , fireDataEvent , fireErrorEvent){
+        
+        axios(params)
+            .then(({
+                data
+            }) => fireDataEvent(data))
+            .catch(({
+                status,
+                data
+            }) => fireErrorEvent({
+                status,
+                data
+            }));
     }
 
     processSendParams(params){
