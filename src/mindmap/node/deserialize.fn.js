43eb9739af5236c54150f9803186176c421115ca
陈治文 <cheng022074@qq.com>
2020-09-17 09:44:20 +0800

    脑图节点序列化与反序列化方法创建

---
+++ src/mindmap/node/deserialize.fn.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * 脑图节点反序列化
+ * 
+ * @param {mixed} node 脑图节点
+ * 
+ * @param {object} data 序列化数据
+ * 
+ */
+
+ // 代码实现
---
+++ src/mindmap/node/serialize.fn.js
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ * 脑图节点序列化
+ * 
+ * @param {mixed} node 脑图节点
+ * 
+ * @return {object} 序列化后的数据 
+ * 
+ */
+
+ return {} ;
