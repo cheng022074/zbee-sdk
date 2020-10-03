46004e3cc5e5ca74cdce70e2e2e8a25b8d0f0406
陈治文 <cheng022074@qq.com>
2020-10-03 10:22:02 +0800

    SPA开发环境配置

--- src/webpack/spa/development.fn.js
+++ src/webpack/spa/development.fn.js
@@ -3,8 +3,46 @@
  * 
  * 单页面 Webpack 开发配置
  * 
+ * @import base from .base
+ * 
+ * @require webpack-merge
+ * 
+ * @require html-webpack-plugin
+ * 
+ * @require style-loader
+ * 
+ * @require css-loader
+ * 
  * @return {object} Webpack 配置 
  * 
  */
 
- // 代码实现
+const {
+    merge
+  } = require('webpack-merge'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  {
+    join
+  } = require('path');
+  
+  module.exports = merge(base() , {
+    mode:'development',
+    devtool: 'inline-source-map',
+    devServer:{
+      contentBase:'./dist'
+    },
+    plugins:[
+      new HtmlWebpackPlugin({
+          template:join(__dirname , '../public/index.html')
+      })
+    ],
+    module:{
+      rules:[{
+          test:/\.css$/,
+          use:[
+              'style-loader',
+              'css-loader'
+          ]
+      }]
+    }
+  }) ;
