ca3e6d9fe1c84bf2ae86bfb5f054dac56b7202e1
陈治文 <cheng022074@qq.com>
2020-10-03 13:35:12 +0800

    webpack 测试环境调整

--- src/webpack/spa/test.fn.js
+++ src/webpack/spa/test.fn.js
@@ -3,8 +3,55 @@
  * 
  * 单页面 Webpack 测试配置
  * 
+ * @import base from .base
+ * 
+ * @require webpack-merge
+ * 
+ * @require mini-css-extract-plugin
+ * 
+ * @require html-webpack-plugin
+ * 
+ * @require css-loader
+ * 
+ * @param {object} options 配置
+ * 
+ * @param {string} options.context 工程目录
+ * 
  * @return {object} Webpack 配置 
  * 
  */
 
- // 代码实现
+const {
+    merge
+} = require('webpack-merge'),
+MiniCssExtractPlugin = require('mini-css-extract-plugin'),
+HtmlWebpackPlugin = require('html-webpack-plugin'),
+{
+    join
+} = require('path');
+  
+return merge(base({
+    context
+}) , {
+    mode:'production',
+    devtool: 'inline-source-map',
+    module:{
+        rules:[{
+            test:/\.css$/,
+            use:[
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        }]
+    },
+    optimization:{
+        minimize:false
+    },
+    plugins:[
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template:join(context , 'public/index.html'),
+            minify:false
+        })
+    ]
+}) ;
