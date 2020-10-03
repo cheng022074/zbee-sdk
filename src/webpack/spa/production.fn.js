a66bf55b38ec353b90bd013732c0e62b2b594abf
陈治文 <cheng022074@qq.com>
2020-10-03 13:33:09 +0800

    生产配置调整

--- src/webpack/spa/production.fn.js
+++ src/webpack/spa/production.fn.js
@@ -17,6 +17,10 @@
  * 
  * @require css-loader
  * 
+ * @param {object} options 配置
+ * 
+ * @param {string} options.context 工程目录
+ * 
  * @return {object} Webpack 配置 
  * 
  */
@@ -28,41 +32,43 @@ MiniCssExtractPlugin = require('mini-css-extract-plugin'),
 TerserJSPlugin = require('terser-webpack-plugin'),
 OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
 HtmlWebpackPlugin = require('html-webpack-plugin'),
-  {
-    join
-  } = require('path');
+{
+  join
+} = require('path');
   
-  return  merge(WebpackConfig , {
-    mode:'production',
-    optimization: {
-      minimizer: [
-        new TerserJSPlugin(),
-        new OptimizeCSSAssetsPlugin()
-      ],
-      splitChunks:{
-        chunks:'all'
-      }
-    },
-    module:{
-      rules:[{
-          test:/\.css$/,
-          use:[
-            MiniCssExtractPlugin.loader,
-            'css-loader'
-          ]
-      }]
-    },
-    plugins:[
-      new HtmlWebpackPlugin({
-          template:join(__dirname , '../public/index.html')
-      }),
-      new MiniCssExtractPlugin()
+return  merge(base({
+  context
+}) , {
+  mode:'production',
+  optimization: {
+    minimizer: [
+      new TerserJSPlugin(),
+      new OptimizeCSSAssetsPlugin()
     ],
-    performance:{
-        hints:false,
-        assetFilter(){
-
-          return false ;
-        } 
+    splitChunks:{
+      chunks:'all'
     }
-  }) ;
+  },
+  module:{
+    rules:[{
+        test:/\.css$/,
+        use:[
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+    }]
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+        template:join(context , 'public/index.html')
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  performance:{
+      hints:false,
+      assetFilter(){
+
+        return false ;
+      } 
+  }
+}) ;
