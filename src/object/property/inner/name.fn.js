9d50f9ebc8f579bb44630810d3d29fa047486bb3
陈治文 <cheng022074@qq.com>
2020-10-23 14:17:51 +0800

    增加属性内部名称类型设置

--- src/object/property/inner/name.fn.js
+++ src/object/property/inner/name.fn.js
@@ -7,8 +7,10 @@
  * 
  * @param {string} name 属性名称
  * 
+ * @param {string} [type] 属性类型名称
+ * 
  * @return {string} 属性内部名称
  * 
  */
 
- return `__${id}_OBJECT_INNER_PROPERTY_${name}__` ;
+ return `__${id}_${type ? type : 'OBJECT_INNER_PROPERTY'}_${name}__` ;
