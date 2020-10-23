7788313be63532829ab3072b2e5e0f13d0361c94
陈治文 <cheng022074@qq.com>
2020-10-23 16:48:38 +0800

    恢复内部属性名称设置

--- src/object/property/inner/name.fn.js
+++ src/object/property/inner/name.fn.js
@@ -7,10 +7,8 @@
  * 
  * @param {string} name 属性名称
  * 
- * @param {string} [type] 属性类型名称
- * 
  * @return {string} 属性内部名称
  * 
  */
 
- return `__${id}_${type ? type : 'OBJECT_INNER_PROPERTY'}_${name}__` ;
+ return `__${id}_OBJECT_INNER_PROPERTY_${name}__` ;
