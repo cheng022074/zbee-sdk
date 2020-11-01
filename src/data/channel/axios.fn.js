3168efe50376d5883f7f691cf81c46a0c84729d6
cheng022074 <cheng022074@qq.com>
2020-11-01 10:23:41 +0800

    基于数据通道的 axios 实现

---
+++ src/data/channel/axios.fn.js
@@ -0,0 +1,80 @@
+
+/**
+ * 
+ * 基于 axios 通用客户端单向数据通道
+ * 
+ * @import Channel from data.channel
+ * 
+ * @import on from event.listener.add
+ * 
+ * @import off from event.listener.remove
+ * 
+ * @import axios
+ * 
+ * @class
+ * 
+ */
+
+class main extends Channel{
+
+    constructor(receivers){
+
+        super(receivers) ;
+
+        me.receiveConnected() ;
+
+        me.cancelTokens = {} ;
+    }
+
+    send(params){
+
+        return super.send(null , params) ;
+    }
+
+    processSendParams(params){
+
+        return params ;
+    }
+
+    doSend({
+        type,
+        id,
+        params
+    }){
+
+        let me = this,
+        {
+            cancelTokens
+        } = me;
+
+        switch(type){
+
+            case 'send':
+
+                cancelTokens[id] = axios(me.processSendParams(params) , (isSuccess , value) => {
+
+                    if(isSuccess){
+
+                        me.receiveReplyValue({
+                            id,
+                            value
+                        }) ;
+                    }
+
+                }) ;
+
+            break ;
+
+            case 'cancel-send':
+
+                if(cancelTokens.hasOwnProperty(id)){
+
+                    cancelTokens[id]() ;
+
+                    delete cancelTokens[id] ;
+                }
+        }
+
+        
+    }
+}
