717d029efc8ac8f5dd807f2142afaec1686d4721
陈治文 <cheng022074@qq.com>
2020-11-26 18:22:51 +0800

    隐藏启动

--- src/mindmap/hidden/after.fn.js
+++ src/mindmap/hidden/after.fn.js
@@ -3,11 +3,22 @@
  * 
  * 设置隐藏属性之后调用
  * 
- * @import unsized from ..node.unsized.register scoped
+ * @import register from ..node.unsized.register scoped
+ * 
+ * @import unregister from ..node.unsized.unregister scoped
  * 
  * @param {data.Record} node 脑图节点
  * 
  */
 
-unsized(node) ;
+ if(node.hidden){
+
+    unregister(node) ;
+ 
+ }else{
+
+    register(node) ;
+ }
+
+
  
