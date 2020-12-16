c5a97811f6f2adc4ba77ced22084cf20563b13d9
陈治文 <cheng022074@qq.com>
2020-12-16 11:01:09 +0800

    公布 loadData 方法

--- src/mindmap/load.fn.js
+++ src/mindmap/load.fn.js
@@ -3,13 +3,7 @@
  * 
  * 初始化脑图数据
  * 
- * @import initSortNodes from .load.sort scoped
- * 
- * @import initVisibilityNodes from .load.visibility scoped
- * 
- * @import initNodes from .load.nodes scoped
- * 
- * @import tryLayout from .layout.try scoped
+ * @import loadData from .load.data scoped
  * 
  * @param {mixed} data 数据
  * 
@@ -20,22 +14,6 @@
     reader,
     readConfig,
     callback
- } = me,
- result = reader.read(data , readConfig) ;
+ } = me;
 
- if(result.length === 1){
-
-    let rootNode = me.rootNode = result[0] ;
-
-    rootNode.selected = true ;
-
-    initSortNodes() ;
-
-    initNodes() ;
-
-    initVisibilityNodes() ;
-
-    await tryLayout() ;
-
-    callback('load') ;
- }
+ await loadData(reader.read(data , readConfig)) ;
---
+++ src/mindmap/load/data.fn.js
@@ -0,0 +1,33 @@
+
+/**
+ * 
+ * 加载脑图专有数据
+ * 
+ * @import initSortNodes from .sort scoped
+ * 
+ * @import initVisibilityNodes from .visibility scoped
+ * 
+ * @import initNodes from .nodes scoped
+ * 
+ * @import tryLayout from ..layout.try scoped
+ * 
+ * @param {array} data 脑图数据
+ * 
+ */
+
+if(data.length === 1){
+
+    let rootNode = this.rootNode = data[0] ;
+
+    rootNode.selected = true ;
+
+    initSortNodes() ;
+
+    initNodes() ;
+
+    initVisibilityNodes() ;
+
+    await tryLayout() ;
+
+    callback('load') ;
+ }
