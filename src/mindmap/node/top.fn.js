8d7350753feeea6698fde5a76ea954cfa32ea6ec
陈治文 <cheng022074@qq.com>
2020-12-31 13:20:38 +0800

    脑图节点顶部值

---
+++ src/mindmap/node/top.fn.js
@@ -0,0 +1,37 @@
+
+/**
+ * 
+ * 获得脑图节点顶部值
+ * 
+ * @import getNodeSeparationDistance from .distance.separation scoped
+ * 
+ * @import data from .data scoped
+ * 
+ * @import getParentNode from .parent scoped
+ * 
+ * @import isRootNode from .is.root scoped
+ * 
+ * @param {data.Record} node 脑图节点
+ * 
+ * @param {boolean} [isBodySize = true] 是否仅计算节点本身尺寸，默认为 true
+ * 
+ * @return {mixed} 返回说明 
+ * 
+ */
+
+ let {
+    y,
+    hidden
+ } = node ;
+
+ if(!hidden){
+
+    if(isBodySize){
+
+        return y ;
+    }
+
+    return y - getNodeSeparationDistance(data(node) , getParentNode(node).children.indexOf(node)) ;
+ }
+
+ return NaN ;
