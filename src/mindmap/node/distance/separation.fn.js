fac03689f390394342a07528b99762c21e9bc793
陈治文 <cheng022074@qq.com>
2020-12-31 14:04:23 +0800

    参数调整

--- src/mindmap/node/distance/separation.fn.js
+++ src/mindmap/node/distance/separation.fn.js
@@ -7,13 +7,13 @@
  * 
  * @import is.function
  * 
- * @import {string} direction 相对于脑图节点间隔所在的方向
+ * @param {string} direction 相对于脑图节点间隔所在的方向
  * 
  * @param {object} node 脑图节点数据
  * 
  * @param {number} index 脑图节点在父节点的子节点集合中的位置
  * 
- * @import {number} length 脑图节点所在的父节点的子节点集合总数
+ * @param {number} length 脑图节点所在的父节点的子节点集合总数
  * 
  * @return {number} 返回脑图节点之间的间隔值 
  * 
