25fa0ba8aab5e51b7efcd9538ae198d3007dc953
陈治文 <cheng022074@qq.com>
2021-01-04 10:22:32 +0800

    基于范围获取宽与高

--- src/math/region/height.fn.js
+++ src/math/region/height.fn.js
@@ -3,14 +3,17 @@
  * 
  * 获得方位高度
  * 
+ * @import get from .get
+ * 
  * @param {object} region 范围
- * 
- * @param {number} region.top 范围顶
- * 
- * @param {number} region.bottom 范围底
  * 
  * @return {number} 宽度值 
  * 
  */
+
+ let {
+    bottom,
+    top
+ } = get(region) ;
 
 return bottom - top ;
