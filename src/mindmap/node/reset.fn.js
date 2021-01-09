726d2c173721dc9d571a7824da1ed0748d97c023
cheng022074 <cheng022074@qq.com>
2021-01-09 16:29:25 +0800

    重置脑图节点

---
+++ src/mindmap/node/reset.fn.js
@@ -0,0 +1,48 @@
+
+/**
+ * 
+ * 重置脑图
+ * 
+ * @import getDescendantNodes from ..nodes.relation.descendant scoped
+ * 
+ * @import expand from .expand.deep scoped
+ * 
+ * @import collapse from .collapse.deep scoped
+ * 
+ * @param {mixed} rootNode 脑图节点
+ * 
+ * @param {number} [level = 1] 显示层数
+ * 
+ * @return {boolean} 如果发生重置行为则返回 true , 否则返回 false
+ * 
+ */
+
+ rootNode = from(rootNode) ;
+
+let isReset = false;
+
+if(expand(rootNode , level)){
+
+    isReset = true ;
+}
+
+maxNodeLevel += level ;
+
+let nodes = getDescendantNodes(rootNode) ;
+
+for(let node of nodes){
+
+    let {
+        level:nodeLevel
+    } = node ;
+
+    if(nodeLevel === maxNodeLevel){
+
+        if(collapse(node)){
+
+            isReset = true ;
+        }
+    }
+}
+
+return isReset ;
--- src/mindmap/reset.fn.js
+++ src/mindmap/reset.fn.js
@@ -5,40 +5,22 @@
  * 
  * @import getRootNode from .node.root scoped
  * 
- * @import getParentNode from .node.parent scoped
+ * @import reset from .node.reset scoped
+ * 
+ * @import select from .select scoped
  * 
  * @param {number} [level] 显示层数
  * 
  */
 
- let me = this,
- {
-    nodes
- } = me,
- rootNode = getRootNode() ;
+let rootNode = getRootNode(),
+    isSelect = select(rootNode),
+    isReset = reset(rootNode , level) ;
 
+if(!isSelect && isReset){
 
+    this.layout() ;
+}
 
- me.select(rootNode) ;
-
- nodes.forEach(node => {
-
-    if(node.selected){
-
-        return ;
-    
-    }else if(getParentNode(node) === rootNode){
-
-        node.expanded = false ;
-    
-    }else{
-
-        node.hidden = true ;
-
-        node.expanded = false ;
-    }
- }) ;
-
- me.layout() ;
 
 
