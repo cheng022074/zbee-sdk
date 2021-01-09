361f2dc87ae6940c94d74821d3b8424e3452b703
cheng022074 <cheng022074@qq.com>
2021-01-09 21:39:25 +0800

    修复深度收起 API

--- src/mindmap/node/collapse/deep.fn.js
+++ src/mindmap/node/collapse/deep.fn.js
@@ -3,6 +3,8 @@
  * 
  * 深度收起
  * 
+ * @import from from ..from scoped
+ * 
  * @import getDescendantNodes from ....data.nodes.descendant scoped
  * 
  * @param {mixed} node 脑图节点
@@ -10,25 +12,34 @@
  * 
  */
 
- let nodes = getDescendantNodes(node),
-     isCollapse = false;
+ node = from(node) ;
 
- for(let node of nodes){
-
-    let {
-        children,
-        expanded,
-        hidden
-    } = node ;
-
-    if(!hidden && children.length && expanded){
-
-        isCollapse = true ;
-    }
+ if(!node.hidden){
 
     node.expanded = false ;
 
-    node.hidden = false ;
+    let nodes = getDescendantNodes(node),
+        isCollapse = false;
+
+    for(let node of nodes){
+
+        let {
+            children,
+            expanded,
+            hidden
+        } = node ;
+
+        if(!hidden && children.length && expanded){
+
+            isCollapse = true ;
+        }
+
+        node.expanded = false ;
+
+        node.hidden = true ;
+    }
+
+    return isCollapse ;
  }
 
- return isCollapse ;
+ return false ;
