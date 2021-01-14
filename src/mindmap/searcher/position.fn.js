d4f8969a2c611524e15a1f551c9af02a7a47f9ba
陈治文 <cheng022074@qq.com>
2021-01-14 18:09:01 +0800

    空实现

---
+++ src/mindmap/searcher/position.fn.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * 脑图位置搜索器
+ * 
+ */
+
+ class main{
+
+    constructor(){
+
+
+    }
+
+    refresh(){
+
+
+    }
+
+    getNearestNode(region){
+
+        
+    }
+
+
+    find(region){
+
+
+    }
+
+ }
