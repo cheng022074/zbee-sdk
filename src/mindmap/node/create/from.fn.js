b53b3e2915f668801e97f3b8542b77e3c6bc06f9
陈治文 <cheng022074@qq.com>
2021-01-19 12:48:05 +0800

    可解析不同的根节点设置

--- src/mindmap/node/create/from.fn.js
+++ src/mindmap/node/create/from.fn.js
@@ -3,7 +3,11 @@
  * 
  * 以外部数据创建脑图节点
  * 
+ * @import is.defined
+ * 
  * @param {array} data 脑图节点数据
+ * 
+ * @param {mixed} [readAsRoot] 获得脑图节点根读取入口
  * 
  * @return {data.Record} 创建后的脑图节点 
  * 
@@ -14,6 +18,13 @@ let me = this,
    reader,
    readConfig
 } = me ;
+
+if(isDefined(readAsRoot)){
+
+  readConfig = Object.assign({} , readConfig , {
+    root:readAsRoot
+  }) ;
+}
 
 return reader.read(data , {
   ...readConfig,
