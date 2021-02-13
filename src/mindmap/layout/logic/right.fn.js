8fec0bacb69a83b6cd15d842662a51c05c3c15ed
cheng022074 <cheng022074@qq.com>
2021-02-13 22:05:19 +0800

    往右逻辑脑图布局算法实现

--- src/mindmap/layout/logic/right.fn.js
+++ src/mindmap/layout/logic/right.fn.js
@@ -3,9 +3,46 @@
  * 
  * 向右逻辑图算法实现
  * 
- * @param {data.Record} node 进行布局的根节点
+ * @import getChildNodes from ..nodes.child scoped
+ * 
+ * @import createRegion from .region
+ * 
+ * @import getHeight from math.region.width
+ * 
+ * @import setAnchorY from math.region.y.anchor
+ * 
+ * @import getY from match.region.y
+ * 
+ * @import from from math.from
+ * 
+ * @param {data.Record} rootNode 布局的根节点
  * 
  */
 
- 
+ function main(node){
+
+    let region = createRegion(node) ;
+
+    layout(node , region) ;
+ }
+     
+ function layout(node , region){
+
+    let childNodes = getChildNodes(node) ;
+
+    for(let childNode of childNodes){
+
+        layout(childNode) ;
+
+
+    }
+
+    let nodeRegion = from(node) ;
+
+    setAnchorY(nodeRegion , getHeight(region.children(node)) / 2 , 'center') ;
+
+    node.y = getY(nodeRegion) ;
+
+    
+ }
 
