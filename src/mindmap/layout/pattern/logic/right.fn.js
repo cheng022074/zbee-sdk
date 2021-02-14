6d29cbd8c4076872ddb2587a15e2859cf3760a60
陈治文 <cheng022074@qq.com>
2021-02-14 21:04:22 +0800

    布局模式 - 右侧逻辑图

--- src/mindmap/layout/pattern/logic/right.fn.js
+++ src/mindmap/layout/pattern/logic/right.fn.js
@@ -3,23 +3,23 @@
  * 
  * 向右逻辑图算法实现
  * 
- * @import setX from ..node.x scoped
+ * @import setX from ....node.x scoped
  * 
- * @import setY from ..node.y scoped
+ * @import setY from ....node.y scoped
  * 
- * @import setOffsetY from ..node.y.offset scoped
+ * @import setOffsetY from ....node.y.offset scoped
  * 
- * @import getPreviousSiblingNode from ..node.sibling.previous scoped
+ * @import getPreviousSiblingNode from ....node.sibling.previous scoped
  * 
- * @import getParentNode from ..node.parent scoped
+ * @import getParentNode from ....node.parent scoped
  * 
- * @import getChildNodes from ..nodes.child scoped
+ * @import getChildNodes from ....nodes.child scoped
  * 
- * @import getDescendantRegion from ..node.region.descendant.logic scoped
+ * @import getDescendantRegion from ....node.region.descendant.logic scoped
  * 
- * @import getSelfRegion from ..node.region.self scoped
+ * @import getSelfRegion from ....node.region.self scoped
  * 
- * @import getChildRegion from ..node.region.child.logic scoped
+ * @import getChildRegion from ....node.region.child.logic scoped
  * 
  * @import getHeight from math.region.width
  * 
