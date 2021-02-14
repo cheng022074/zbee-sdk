c1f3a7799a5bf84685779c5e619dfdbd6746ea39
陈治文 <cheng022074@qq.com>
2021-02-14 20:26:05 +0800

    添加排序数组项

---
+++ debug/array/sort/add.js
@@ -0,0 +1,37 @@
+/**
+ * 
+ * @import add from array.add.sort
+ * 
+ */
+
+ let data = [] ;
+
+ const sortFn = (a , b) => a - b ;
+
+ add(data , 3 , sortFn) ;
+
+ add(data , 2 , sortFn) ;
+
+ add(data , 5 , sortFn) ;
+
+ add(data , 10 , sortFn) ;
+
+ add(data , 7 , sortFn) ;
+
+ add(data , 1 , sortFn) ;
+
+ add(data , 4 , sortFn) ;
+
+ add(data , 6 , sortFn) ;
+
+ add(data , 9 , sortFn) ;
+
+ add(data ,11 , sortFn) ;
+
+ add(data , -6 , sortFn) ;
+
+ add(data , 120 , sortFn) ;
+
+ add(data , -120 , sortFn) ;
+
+ console.log(data) ;
--- src/array/add/sort.fn.js
+++ src/array/add/sort.fn.js
@@ -13,27 +13,54 @@
 
  function main(data , item , sortFn){
 
-    data.splice(getInsertIndex(data , item , sortFn , 0 , data.length - 1) , 0 , item) ;
+    let {
+        length
+    } = data ;
+
+    if(length === 0){
+
+        data.push(item) ;
+    
+    }else{
+
+        data.splice(getInsertIndex(data , item , sortFn , 0 , data.length - 1) , 0 , item) ;
+    }
  }
 
  function getInsertIndex(data , item , sortFn , start , end){
 
-    let index = (end - start) / 2,
-        currentItem = data[index],
+    let index ;
+
+    if(start === end){
+
+        index = start ;
+    
+    }else{
+
+        index = start + Math.round((end - start) / 2) ;
+    }
+
+    let currentItem = data[index],
         value = sortFn(currentItem , item);
 
     if(value <= 0){
 
-        start = index ;
+        start = index + 1;
+
+        if(start > end || sortFn(data[start] , item) > 0){
+
+            return index + 1;
+        
+        }
     
     }else if(value > 0){
 
         end = index - 1 ;
-    }
 
-    if(start > end){
+        if(start > end){
 
-        return index + 1;
+            return index === 0 ? index : index - 1;
+        }
     }
 
     return getInsertIndex(data , item , sortFn , start , end) ;
