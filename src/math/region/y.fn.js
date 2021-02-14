c4002f4f7c51ec579a38e0b1b9b8b323932d061a
陈治文 <cheng022074@qq.com>
2021-02-14 19:07:41 +0800

    获取锚定纵坐标

---
+++ src/math/region/y.fn.js
@@ -0,0 +1,14 @@
+
+/**
+ * 
+ * 获得范围的纵坐标
+ * 
+ * @import getAnchorY from .y.anchor
+ * 
+ * @param {object} region 范围
+ * 
+ * @return {number} 纵坐标值
+ * 
+ */
+
+ return getAnchorY(region , 'top') ;
