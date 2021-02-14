236c1e740c554ba7216c8446d5f641bc7fe87731
陈治文 <cheng022074@qq.com>
2021-02-14 09:32:30 +0800

    用于布局的缓存

---
+++ src/mindmap/layout/cache.fn.js
@@ -0,0 +1,39 @@
+
+/**
+ * 
+ * 布局缓存
+ * 
+ * @param {data.Record} node 脑图节点
+ * 
+ * @param {string} cacheName 缓存名称
+ * 
+ * @param {function} cacheFn 缓存计算函数
+ * 
+ * @return {mixed} 缓存返回值
+ * 
+ */
+
+ let me = this ;
+
+ if(!me.hasOwnProperty('layoutCache')){
+
+    me.layoutCache = new Map() ;
+ }
+
+ let {
+    layoutCache
+ } = me ;
+
+ if(!layoutCache.has(node)){
+
+    layoutCache.set(node , {}) ;
+ }
+
+ let cache = layoutCache.get(node) ;
+
+ if(!cache.hasOwnProperty(cacheName)){
+
+    cache[cacheName] = cacheFn(node) ;
+ }
+
+ return cache[cacheName] ;
