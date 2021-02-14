040a79174d3f05bbe637099039f254de1c059444
陈治文 <cheng022074@qq.com>
2021-02-14 17:57:34 +0800

    获得脑图节点自身范围

---
+++ src/mindmap/layout/node/region/self.fn.js
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * 获得脑图节点自身范围
+ * 
+ * @import getLeftSpacing from .spacing.left scoped
+ * 
+ * @import getRightSpacing from .spacing.right scoped
+ * 
+ * @import getTopSpacing from .spacing.top scoped
+ * 
+ * @import getBottomSpacing from .spacing.bottom scoped
+ * 
+ * @param {data.Record} node 脑图节点
+ * 
+ * @return {object} 范围信息 
+ * 
+ */
+
+ let {
+    x,
+    y,
+    width,
+    height
+ } = node,
+ top = y - getTopSpacing(node),
+ left = x - getLeftSpacing(node);
+
+ return {
+     top,
+     bottom:top + height + getBottomSpacing(node),
+     left,
+     right:left + width + getRightSpacing(node)
+ } ;
+
+
