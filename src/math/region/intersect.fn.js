b89a19e23b9146a53985f12b80a81d00dd292f50
陈治文 <cheng022074@qq.com>
2021-02-14 20:59:13 +0800

    判断范围是否相交

--- src/math/region/intersect.fn.js
+++ src/math/region/intersect.fn.js
@@ -1,12 +1,14 @@
 
 /**
  * 
- * 函数实现说明
+ * 判断两个范围是否相交
  * 
- * @param {mixed} data 参数说明
+ * @param {object} region1 范围1
  * 
- * @return {mixed} 返回说明 
+ * @param {object} region2 范围2
+ * 
+ * @return {boolean} 如果两者相交则返回 true , 否则返回 false
  * 
  */
 
- // 代码实现
+return min(region1.bottom , region2.bottom) > max(region1.top , region2.top) && min(region1.right , region2.right) > max(region1.left , region2.left) ;
