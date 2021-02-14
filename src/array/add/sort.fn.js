1bbd786aaa8a8a9f168ab6652e7a1ff006a2bc85
陈治文 <cheng022074@qq.com>
2021-02-14 19:49:13 +0800

    以排序方式插入

--- src/array/add/sort.fn.js
+++ src/array/add/sort.fn.js
@@ -1,12 +1,40 @@
 
 /**
  * 
- * 函数实现说明
+ * 以排序的形式添加数组项
  * 
- * @param {mixed} data 参数说明
+ * @param {array} data 数组
  * 
- * @return {mixed} 返回说明 
+ * @param {mixed} item 添加项
+ * 
+ * @param {function} sortFn 排序函数
  * 
  */
 
- // 代码实现
+ function main(data , item , sortFn){
+
+    data.splice(getInsertIndex(data , item , sortFn , 0 , data.length - 1) , 0 , item) ;
+ }
+
+ function getInsertIndex(data , item , sortFn , start , end){
+
+    let index = (end - start) / 2,
+        currentItem = data[index],
+        value = sortFn(currentItem , item);
+
+    if(value <= 0){
+
+        start = index ;
+    
+    }else if(value > 0){
+
+        end = index - 1 ;
+    }
+
+    if(start > end){
+
+        return index + 1;
+    }
+
+    return getInsertIndex(data , item , sortFn , start , end) ;
+ }
