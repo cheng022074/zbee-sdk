2ba231c3395957a4b24336f516985b4c34d9b1b8
陈治文 <cheng022074@qq.com>
2021-02-14 08:06:26 +0800

    初步布局算法建立

--- src/mindmap/layout/logic/right.fn.js
+++ src/mindmap/layout/logic/right.fn.js
@@ -3,9 +3,23 @@
  * 
  * 向右逻辑图算法实现
  * 
+ * @import setX from ..node.x scoped
+ * 
+ * @import setY from ..node.y scoped
+ * 
+ * @import setOffsetY from ..node.y.offset scoped
+ * 
+ * @import getPreviousSiblingNode from ..node.sibling.previous scoped
+ * 
+ * @import getParentNode from ..node.parent scoped
+ * 
  * @import getChildNodes from ..nodes.child scoped
  * 
- * @import createRegion from .region
+ * @import getFirstDescendantNodes from ..nodes.descendant.first scoped
+ * 
+ * @import getNodeRegion from ....region.node scoped
+ * 
+ * @import getChildRegion from ....region.logic.child scoped
  * 
  * @import getHeight from math.region.width
  * 
@@ -15,34 +29,89 @@
  * 
  * @import from from math.from
  * 
+ * @import add from array.add.sort
+ * 
+ * @import intersect from math.region.intersect
+ * 
  * @param {data.Record} rootNode 布局的根节点
  * 
  */
 
  function main(node){
 
-    let region = createRegion(node) ;
-
-    layout(node , region) ;
+    layout(node) ;
  }
      
- function layout(node , region){
+ function layout(node , positionedRegions = []){
 
     let childNodes = getChildNodes(node) ;
 
     for(let childNode of childNodes){
 
-        layout(childNode) ;
-
+      layout(childNode , positionedRegions) ;
 
     }
 
     let nodeRegion = from(node) ;
 
-    setAnchorY(nodeRegion , getHeight(region.children(node)) / 2 , 'center') ;
+    setAnchorY(nodeRegion , getHeight(getChildRegion(node)) / 2 , 'center') ;
 
     node.y = getY(nodeRegion) ;
 
+    let previousSiblingNode = getPreviousSiblingNode(node) ;
+
+    if(previousSiblingNode){
+
+      setY(node , getNodeRegion(previousSiblingNode).bottom) ;
+
+      adjustY(node , getFirstDescendantNodes(node) , positionedRegions) ;
     
+    }
+
+    adjustX(node , getParentNode(node)) ;
+
+    add(positionedRegions , getNodeRegion(node) , sortPositionedRegions) ;
+ }
+
+ function adjustX(node , parentNode){
+
+   if(parentNode){
+
+      setX(node , getNodeRegion(parentNode).right) ;
+   }
+ }
+
+ function adjustY(node , firstDescendantNodes , positionedRegions){
+
+   for(let firstDescendantNode of firstDescendantNodes){
+
+      let region = getNodeRegion(firstDescendantNode),
+      {
+         top
+      } = region;
+
+      for(let positionedRegion of positionedRegions){
+
+         let {
+            bottom
+         } = positionedRegion ;
+
+         if(top > bottom && intersect(region , positionedRegion)){
+
+            setOffsetY(node , top - bottom) ;
+
+            break ;
+         }
+      }
+   }
+ }
+
+ function sortPositionedRegions({
+    bottom:bottom1
+ } , {
+    bottom:bottom2
+ }){
+
+   return bottom1 - bottom2 ;
  }
 
