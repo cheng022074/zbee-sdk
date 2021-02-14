677896aff6a31e760ec066e576473446b938c07c
陈治文 <cheng022074@qq.com>
2021-02-14 09:09:52 +0800

    设置脑图节点横坐标

--- src/mindmap/layout/node/x.fn.js
+++ src/mindmap/layout/node/x.fn.js
@@ -1,12 +1,37 @@
 
 /**
  * 
- * 函数实现说明
+ * 设置节点的横坐标
  * 
- * @param {mixed} data 参数说明
+ * @import getHorizontalSpacing from .spacing.horizontal scoped
  * 
- * @return {mixed} 返回说明 
+ * @import getChildNodes from ..nodes.child scoped
+ * 
+ * @import setOffsetX from .x.offset scoped
+ * 
+ * @param {data.Record} node 脑图节点
+ * 
+ * @param {number} x 节点横坐标
+ * 
+ * @param {boolean} [recursive = true] 是否递归设置横坐标
  * 
  */
 
- // 代码实现
+ let value = x + getHorizontalSpacing(node , 'left'),
+ {
+    x:oldValue = 0
+ } = node;
+
+node.x = value ;
+
+let offsetValue = value - oldValue ;
+
+if(recursive){
+
+    let childNodes = getChildNodes(node) ;
+
+    for(let childNode of childNodes){
+
+        setOffsetX(childNode , offsetValue) ;
+    }
+}
