64b5d331d393e1f94e17f47edb6b064a3fc8ea3b
陈治文 <cheng022074@qq.com>
2021-02-15 11:54:34 +0800

    获取节点的锚点坐标

--- src/mindmap/refresh.fn.js
+++ src/mindmap/refresh.fn.js
@@ -3,8 +3,53 @@
  * 
  * 刷新
  * 
- * @import fireDrawEvent from .fire.draw scoped
+ * @import getData from .layout.node.data.draw scoped
+ * 
+ * @import from from math.region.from
+ * 
+ * @import getAnchorXY from math.region.xy.anchor
  * 
  */
 
-fireDrawEvent() ;
+ function main(){
+
+    let me = this,
+    {
+        layoutNodes,
+        layoutData
+    } = me,
+    {
+        size,
+        offset
+    } = layoutData;
+
+    me.fireEvent('draw' , {
+        nodes:nodes(layoutNodes , offset),
+        lines:lines(layoutNodes),
+        selectedNode:selectedNode(layoutNodes),
+        canvas:size
+    }) ;
+ }
+
+ function getNodes(nodes , offset){
+
+    let result = new Map() ;
+
+    for(let node of nodes){
+
+        let data = getData(node , offset),
+            region = from(data);
+
+        result.set(node , {
+            data,
+            centerXY:getAnchorXY(region , 'c'),
+            rightXY:getAnchorXY(region , 'r'),
+            leftXY:getAnchorXY(region , 'l')
+        })
+    }
+
+    return result ;
+ }
+
+
+
