e76107ae1ffa16f5f826abbdb4cea4842a743b4a
陈治文 <cheng022074@qq.com>
2021-02-15 13:47:27 +0800

    布局之前检查参与布局的节点是否都拥有尺寸

--- src/mindmap/layout.fn.js
+++ src/mindmap/layout.fn.js
@@ -5,24 +5,99 @@
  * 
  * @import refresh from .refresh scoped
  * 
+ * @import isUnsized from .node.is.unsized scoped
+ * 
+ * @import getData from .layout.node.data.param scoped
  * 
  */
 
- const me = this,
- {
-    pattern:layout,
-    getRootNode,
-    getDescendantNodes
- } = me.layoutConfig ;
+ function main(){
 
- let rootNode = getRootNode() ;
+   let me = this,
+   {
+      isLayouting
+   } = me;
 
- me.layoutNodes = [
-    rootNode,
-    ...getDescendantNodes(rootNode)
- ] ;
+   if(isLayouting){
 
- me.layoutData = layout(rootNode) ;
+      return ;
+   }
 
- refresh() ;
- 
+   me.isLayouting = true ;
+
+   let {
+      pattern:layout,
+      getRootNode,
+      getDescendantNodes
+   } = me.layoutConfig ;
+
+   let rootNode = getRootNode() ;
+
+   doBeforeLayout(me.layoutNodes = [
+      rootNode,
+      ...getDescendantNodes(rootNode)
+   ] , () => {
+
+      me.layoutData = layout(rootNode) ;
+
+      refresh() ;
+
+      me.isLayouting = false ;
+
+   }) ;
+ }
+
+ function doBeforeLayout(layoutNodes , callback){
+
+   let unsizedNodes = new Map() ;
+
+    for(let layoutNode of layoutNodes){
+
+      if(isUnsized(layoutNode)){
+
+         unsizedNodes.set(layoutNode.id , layoutNode) ;
+      }
+    }
+
+    if(unsizedNodes.size){
+
+      me.fireEvent('nodeunsized' , getDataNodes(unsizedNodes) , sizes => {
+
+         let ids = Object.keys(sizes) ;
+
+         for(let id of ids){
+
+            let node = unsizedNodes.get(id),
+            {
+               width,
+               height
+            } = sizes[id];
+
+            node.width = width ;
+
+            node.height = height ;
+         }
+
+         callback() ;
+
+      }) ;
+
+    }else{
+
+      callback() ;
+    }
+ }
+
+ function getDataNodes(nodes){
+
+   nodes = nodes.values() ;
+
+   let result = [] ;
+
+   for(let node of nodes){
+
+      result.push(getData(node)) ;
+   }
+
+   return result ;
+ }
