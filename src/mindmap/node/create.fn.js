7dc17b9626c5a214f72eec2de82295f949322d3e
陈治文 <cheng022074@qq.com>
2021-02-18 14:16:44 +0800

    纯粹的创建节点

--- src/mindmap/node/create.fn.js
+++ src/mindmap/node/create.fn.js
@@ -5,51 +5,16 @@
  * 
  * @import generate from .id.generate
  * 
- * @import is from is.data.record
- * 
- * @import isVisibilityNode from .is.visibility
- * 
- * @import removeNode from .delete scoped
- * 
- * @import remove from array.remove
- * 
- * @import create from .create scoped
- * 
- * @import from from ..data.node.from scoped
- * 
- * @param {mixed} node 节点信息
- * 
- * @param {mixed} [parentNode] 父节点
+ * @param {object} node 节点信息
  * 
  * @return {data.Record} 创建出来的新节点 
  * 
  */
 
-parentNode = from(parentNode) ;
-
 let {
    reader,
    nodes
 } = this ;
-
-if(is(node)){
-
-   removeNode(node) ;
-
-   if(parentNode){
-
-      node.parentNodeId = parentNode.id ;
-   }
-
-   return node ;
-}
-
-let existNode = from(node);
-
-if(existNode){
-
-   return create(existNode , parentNode) ;
-}
 
 delete node.hidden ;
 
@@ -60,11 +25,6 @@ let options = {
    ...node,
    children:[]
 } ;
-
-if(parentNode){
-
-   options.parentNodeId = parentNode.id ;
-}
 
 node = reader.create(options) ;
 
