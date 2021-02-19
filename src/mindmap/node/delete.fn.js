dd32eae8c73aebea499d249e89db2c9eeb935b60
陈治文 <cheng022074@qq.com>
2021-02-19 10:46:24 +0800

    删除节点

--- src/mindmap/node/delete.fn.js
+++ src/mindmap/node/delete.fn.js
@@ -7,11 +7,19 @@
  * 
  * @import getParentNode from .parent scoped
  * 
+ * @import getPreviousNode from .sibling.previous scoped
+ * 
+ * @import getNextNode from .sibling.next scoped
+ * 
  * @import data from .data scoped
  * 
  * @import getDescendantNodes from ..nodes.descendant scoped
  * 
  * @import hide from .hide scoped
+ * 
+ * @import from from .from scoped
+ * 
+ * @import select from .select scoped
  * 
  * @param {mixed} node 节点
  * 
@@ -24,18 +32,30 @@ let parentNode = getParentNode(node) ;
 
 if(!isRootNode(node)){
 
+   select(getPreviousNode(node) || getNextNode(node) || parentNode) ;
+
    hide(node) ;
 
    let nodes = [
        node,
        ...getDescendantNodes(node)
-   ] ;
+   ],
+   me = this,
+   {
+       nodes:originNodes
+   } = me;
 
    for(let node of nodes){
 
        node.parentNodeId = null ;
 
        node.children.length = 0 ;
+
+       node.hidden = false ;
+
+       node.selected = false ;
+
+       originNodes.delete(node.id) ;
    }
 
     let {
