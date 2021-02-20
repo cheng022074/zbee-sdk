5455ba21a6091f956b5749918ec97d7da34e6de6
陈治文 <cheng022074@qq.com>
2021-02-20 21:16:11 +0800

    展开节点并添加

--- src/mindmap/layout/node/drag/move.fn.js
+++ src/mindmap/layout/node/drag/move.fn.js
@@ -13,6 +13,10 @@
  * 
  * @import insertAfter from mindmap.node.insert.after scoped
  * 
+ * @import expand from mindmap.node.expand scoped
+ * 
+ * @import append from mindmap.node.append scoped
+ * 
  * @param {object} xy 坐标
  * 
  * @param {number} xy.y 纵坐标
@@ -50,6 +54,83 @@ if(node){
         return insertBefore(placeholderNode , node) ;
     }
     
+}else{
+
+    let {
+        dragNodeDiscernRadius,
+        placeholderNode
+    } = this,
+    directions = [{
+        direction:'top',
+        property:'y',
+        offset:-dragNodeDiscernRadius
+    },{
+        direction:'bottom',
+        property:'y',
+        offset:dragNodeDiscernRadius
+    },{
+        direction:'left',
+        property:'x',
+        offset:-dragNodeDiscernRadius
+    }];
+
+    for(let {
+        direction,
+        property,
+        offset
+    } of directions){
+
+        let {
+            x,
+            y
+        } = originXY ;
+
+        switch(property){
+
+            case 'x':
+
+                x += offset ;
+
+                break ;
+
+            case 'y':
+
+                y += offset ;
+        }
+
+        let el = document.elementFromPoint(x , y),
+        {
+            id
+        } = el.dataset;
+
+        if(id){
+
+            let node = fromNode(id) ;
+
+            switch(direction){
+
+                case 'top':
+
+                    return insertAfter(placeholderNode , node) ;
+
+                case 'bottom':
+
+                    return insertBefore(placeholderNode , node) ;
+
+                case 'left':
+
+                    expand(node) ;
+
+                    return append(placeholderNode , node);
+
+                
+            }
+
+            break ;
+        }
+    }
+
+
 }
 
 
