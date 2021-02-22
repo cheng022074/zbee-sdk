02e5fbb0bad454e072c42e50e11df76548fe587e
陈治文 <cheng022074@qq.com>
2021-02-22 08:46:13 +0800

    设置坐标

---
+++ src/math/region/xy.fn.js
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ * 平移
+ * 
+ * @import doX from .x
+ * 
+ * @import doY from .y
+ * 
+ * @import get from .get
+ * 
+ * @param {object} region 目标范围
+ * 
+ * @param {object} xy 坐标
+ * 
+ * @param {number} [xy.x] 横坐标
+ * 
+ * @param {number} [xy.y] 纵坐标
+ * 
+ */
+
+ return {
+     x:doX(region , x),
+     y:doY(region , y)
+ } ;
