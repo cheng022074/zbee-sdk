7846fc19cb2ea5b944fdc79694b73da6aab41dda
cheng022074 <cheng022074@qq.com>
2021-02-24 17:13:34 +0800

    节点间距设置

--- src/mindmap/constructor.fn.js
+++ src/mindmap/constructor.fn.js
@@ -113,7 +113,12 @@
 
    if(isObject(nodeSpacing)){
 
-      layoutConfig.nodeSpacing = nodeSpacing ;
+      layoutConfig.nodeSpacing = Object.assign({
+         top:0,
+         bottom:0,
+         left:0,
+         right:0
+      } , nodeSpacing) ;
    
    }
 
