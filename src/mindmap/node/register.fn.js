6ab7304a3f1fc5dc5947ec87d23248db075fdaf8
陈治文 <cheng022074@qq.com>
2021-03-04 17:32:16 +0800

    注册节点

---
+++ src/mindmap/node/register.fn.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * 注册节点
+ * 
+ * @import register from .register scoped
+ * 
+ * @param {data.Record} node 节点
+ * 
+ */
+
+let {
+    id,
+    children
+} = node,
+{
+    nodes
+} = this;
+
+nodes.set(id , node) ;
+
+for(let childNode of children){
+
+    register(childNode) ;
+
+    childNode.parentNodeId = id ;
+}
