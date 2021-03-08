6681864a669b2a8e36cbd62ba1816e60ad4639a4
陈治文 <cheng022074@qq.com>
2021-03-08 13:10:39 +0800

    重新定义移动拦截器

--- src/mindmap/layout/node/is/move/down.fn.js
+++ src/mindmap/layout/node/is/move/down.fn.js
@@ -8,7 +8,11 @@
  * 
  * @import data from mindmap.node.data scoped
  * 
- * @param {function} [onBeforeNodeInsertBefore = () => true] 拖曳的拦截函数 
+ * @param {object} [callbacks = {}] 回调信息
+ * 
+ * @param {function} [callbacks.onBeforeNodeInsertBefore = () => true] 拖曳的拦截函数 
+ * 
+ * @param {function} [callbacks.onBeforeNodeInsertAfter = () => true] 拖曳的拦截函数 
  * 
  * @return {boolean} 判断是否可以向下移动
  * 
@@ -19,6 +23,18 @@ let me = this,
   selectedNode,
   layoutPositioner
 } = me,
-node = next(selectedNode) || layoutPositioner.getMoveDownNode(selectedNode) ;
+node = next(selectedNode) ;
 
-return !! (node && onBeforeNodeInsertBefore(data(getParentNode(node)) , data(selectedNode) , data(node))) ;
+if(node){
+
+  return onBeforeNodeInsertAfter(data(getParentNode(node)) , data(selectedNode) , data(node));
+}
+
+node = layoutPositioner.getMoveDownNode(selectedNode) ;
+
+if(node){
+
+  return onBeforeNodeInsertBefore(data(getParentNode(node)) , data(selectedNode) , data(node)) ;
+}
+
+return false ;
--- src/mindmap/layout/node/is/move/up.fn.js
+++ src/mindmap/layout/node/is/move/up.fn.js
@@ -8,7 +8,11 @@
  * 
  * @import getParentNode from mindmap.node.parent scoped
  * 
- * @param {function} [onBeforeNodeInsertAfter = () => true] 拖曳的拦截函数 
+ * @param {object} [callbacks = {}] 回调信息
+ * 
+ * @param {function} [callbacks.onBeforeNodeInsertBefore = () => true] 拖曳的拦截函数 
+ * 
+ * @param {function} [callbacks.onBeforeNodeInsertAfter = () => true] 拖曳的拦截函数 
  * 
  * @return {boolean} 判断是否可以向下移动
  * 
@@ -18,6 +22,18 @@ let me = this,
    selectedNode,
    layoutPositioner
 } = me,
-node = previous(selectedNode) || layoutPositioner.getMoveUpNode(selectedNode) ;
+node = previous(selectedNode);
 
-return !!(node && onBeforeNodeInsertAfter(data(getParentNode(node)) , data(selectedNode) , data(node))) ;
+if(node){
+
+   return onBeforeNodeInsertBefore(data(getParentNode(node)) , data(selectedNode) , data(node)) ;
+}
+
+node = layoutPositioner.getMoveUpNode(selectedNode) ;
+
+if(node){
+
+   return onBeforeNodeInsertAfter(data(getParentNode(node)) , data(selectedNode) , data(node)) ;
+}
+
+return false ;
