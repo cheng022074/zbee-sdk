319f7e534861ae3d7014a0c8d37be1698a9461aa
陈治文 <cheng022074@qq.com>
2021-04-06 13:39:45 +0800

    初始化 xy-ok-viewport

---
+++ package/xy-ok-viewport/index.prd.js
@@ -0,0 +1,258 @@
+/* eslint-disable */ (function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["XyOkViewport"] = factory();
+	else
+		root["XyOkViewport"] = factory();
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 773:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "include": () => /* binding */ include,
+/* harmony export */   "override": () => /* binding */ override
+/* harmony export */ });
+/* eslint-disable */
+
+const innerExports = {} ;
+
+
+
+const include = (() =>{
+
+    const nameRe = /^(\w+)\:{2}(.+?)$/,
+          CODES = {};
+
+    return name =>{
+
+        if(CODES.hasOwnProperty(name)){
+
+            return CODES[name] ;
+        }
+
+        let match = name.match(nameRe),
+            folder,
+            className;
+    
+        if(match){
+    
+            folder = match[1],
+            className = match[2] ;
+    
+        }else{
+
+            if(innerExports.hasOwnProperty(name)){
+
+                return CODES[name] = innerExports[name] ;
+            }
+    
+            folder = 'src',
+            className = name ;
+        }
+
+        let fullName = `${folder}::${className}`,
+            code = CODES[name] = innerExports[fullName] ;
+
+        if(code === undefined && folder !== 'config'){
+
+            throw new Error(`${fullName} 没有定义`) ;
+        }
+
+        return code ;
+    } ;
+
+})();
+
+const config = (() =>{
+
+    const {
+        freeze:freeze2,
+        isFrozen,
+        keys
+    } = Object ;
+
+    function freeze(data){
+
+        if (data && typeof data === 'object' && !isFrozen(data)){
+
+            freeze2(data);
+
+            let names = keys(data) ;
+
+            for(let name of names){
+
+                freeze(data[name]) ;
+            }
+        }
+
+        return data;
+    }
+
+    const config = {};
+
+    function get_config(target , key){
+
+        if(key){
+    
+            if(target.hasOwnProperty(key)){
+        
+                return freeze(target[key]) ;
+            }
+        
+            let names = key.split(/\./),
+                prefix = '';
+        
+            for(let name of names){
+        
+                let key = `${prefix}${name}` ;
+        
+                if(target.hasOwnProperty(key)){
+        
+                    target = target[key] ;
+        
+                    prefix = '' ;
+                
+                }else{
+        
+                    prefix = `${key}.` ;
+                }
+            }
+
+            if(prefix){
+        
+                return ;
+            }
+        }
+
+        return freeze(target) ; 
+    }
+
+    return (name , key) =>{
+
+        if(config.hasOwnProperty(name)){
+
+            return get_config(config[name] , key) ;
+        }
+
+        return get_config(include(`config::${name}`)() , key) ;
+    }
+
+})();
+
+const override = (() =>{
+
+    const nameRe = /^(\w+)\:{2}(.+?)$/ ;
+
+    return (name , fn) =>{
+
+        if(!nameRe.test(name)){
+
+            name = `src::${name}` ;
+        }
+
+        if(typeof name === 'string' && typeof fn === 'function'){
+    
+            exports[name] = fn ;
+        }
+    } ;
+
+})();
+
+const mixins = ({
+    extend,
+    mixins
+}) =>{
+
+    let baseClass = extend || class {} ;
+
+    if(mixins){
+
+        for(let mixin of mixins){
+
+            baseClass = mixin(baseClass) ;
+        }
+    }
+
+    return baseClass ;
+};
+
+
+
+
+
+
+
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// module exports must be returned from runtime so entry inlining is disabled
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(773);
+/******/ })()
+;
+});
