291825ae59cd5e7e6069e95742f3ccc0d160fda5
陈治文 <cheng022074@qq.com>
2021-04-07 15:45:58 +0800

    增加由外来传入尺寸来计算当前节点位置

--- src/mindmap/layout/node/region/self.fn.js
+++ src/mindmap/layout/node/region/self.fn.js
@@ -11,7 +11,11 @@
  * 
  * @import getBottomSpacing from ..spacing.bottom scoped
  * 
+ * @import isObject from is.object.simple
+ * 
  * @param {data.Record} node 脑图节点
+ * 
+ * @param {object} [size] 指定的脑图节点大小
  * 
  * @return {object} 范围信息 
  * 
@@ -22,8 +26,17 @@
     y,
     width,
     height
- } = node,
- top = y - getTopSpacing(node),
+ } = node ;
+
+ if(isObject(size)){
+
+    width = size.width || width ;
+
+    height = size.height || height ;
+ }
+
+
+ let top = y - getTopSpacing(node),
  left = x - getLeftSpacing(node);
 
  return {
