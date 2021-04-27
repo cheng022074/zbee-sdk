f7b8ce53fb0ee753fca0ff1654f3cf4b52a06c8c
陈治文 <cheng022074@qq.com>
2021-04-27 14:49:37 +0800

    更新发布包

--- package/ok-app/index.asm.js
+++ package/ok-app/index.asm.js
@@ -165,6 +165,395 @@ const mixins = ({
 
 
 
+innerExports['src::object.value.get'] = (() =>{
+
+                    let split,isObject,isArray;
+    
+                    let var_init_locked_1611566404583;
+    
+                    
+
+                    function main(data , key){
+
+        
+/**
+ * 
+ * 获得一个对象的键值
+ * 
+ * @import split from string.split
+ * 
+ * @import is.object
+ * 
+ * @import is.array
+ * 
+ * @param {object} data 对象数据
+ * 
+ * @param {string} [key = '.'] 对象键值
+ * 
+ * @return {mixed} 对应对象数据的键值的数据 
+ * 
+ */
+
+if(key === '.'){
+
+    return data ;
+}
+
+const arrayItemRe1 = /^(\w+)\[(\d+)\]$/,
+      arrayItemRe2 = /^\[(\d+)\]$/;
+
+if(isObject(data) || isArray(data)){
+
+    let keys = split(key , /\./),
+        result;
+
+    for(let key of keys){
+
+        if(arrayItemRe1.test(key) || arrayItemRe2.test(key)){
+
+            {
+                let keyMatch = key.match(arrayItemRe1) ;
+    
+                if(keyMatch){
+    
+                    result = data[keyMatch[1]][Number(keyMatch[2])] ;
+                
+                }
+            }
+    
+            {
+                let keyMatch = key.match(arrayItemRe2) ;
+    
+                if(keyMatch){
+    
+                    result = data[Number(keyMatch[1])] ;
+                
+                }
+            }
+        
+        }else{
+
+            result = data[key] ;
+        }
+
+        if(isObject(result) || isArray(result)){
+
+            data = result ;
+        
+        }else{
+
+            break ;
+        }
+    }
+
+    return result ;
+}
+
+    }
+    
+                    return function(data , key = '.'){
+    
+                        
+        if(!var_init_locked_1611566404583){
+
+            split = include('src::string.split');
+isObject = include('src::is.object');
+isArray = include('src::is.array');
+
+            var_init_locked_1611566404583 = true ;
+        }
+        
+    
+                        return main.call(this , data , key) ;
+                    } ;
+    
+                })();
+
+innerExports['src::is.type'] = (() =>{
+
+                    
+    
+                    
+    
+                    
+
+                    function main(data , type){
+
+        /**
+ * 
+ * 对于 typeof 的简单封装
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @param {string} type 检验数据类型
+ * 
+ * @return {boolean} 如果检验数据的数据类型与检验数据类型一致，则返回 true，否则返回 false 
+ * 
+ */
+
+ return typeof data === type ;
+
+    }
+    
+                    return function(data , type){
+    
+                        
+    
+                        return main.call(this , data , type) ;
+                    } ;
+    
+                })();
+
+innerExports['src::is.array'] = (() =>{
+
+                    let isType;
+    
+                    let var_init_locked_1611566398937;
+    
+                    
+
+                    function main(data){
+
+        /**
+ * 
+ * 判定数据是否为数组类型
+ * 
+ * @import is.type
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @return {boolean} 如果给定值为数组类型则返回 true , 否则返回 false 
+ * 
+ */
+
+ return Array.isArray(data) ;
+
+    }
+    
+                    return function(data){
+    
+                        
+        if(!var_init_locked_1611566398937){
+
+            isType = include('src::is.type');
+
+            var_init_locked_1611566398937 = true ;
+        }
+        
+    
+                        return main.call(this , data) ;
+                    } ;
+    
+                })();
+
+innerExports['src::is.empty'] = (() =>{
+
+                    let isArray;
+    
+                    let var_init_locked_1611566399118;
+    
+                    
+
+                    function main(data , allowEmptyString){
+
+        /**
+ * 
+ * 判定数据是否为空
+ * 
+ * @import is.array
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @param {boolean} [allowEmptyString = false] 是否视空符串不为空，默认空符串为空
+ * 
+ * @return {mixed} 如果给定值为空则返回 true , 否则返回 false  
+ * 
+ */
+
+return (data == null) || (!allowEmptyString ? data === '' : false) || (isArray(data) && data.length === 0);
+
+    }
+    
+                    return function(data , allowEmptyString = false){
+    
+                        
+        if(!var_init_locked_1611566399118){
+
+            isArray = include('src::is.array');
+
+            var_init_locked_1611566399118 = true ;
+        }
+        
+    
+                        return main.call(this , data , allowEmptyString) ;
+                    } ;
+    
+                })();
+
+innerExports['src::string.split'] = (() =>{
+
+                    let isEmpty;
+    
+                    let var_init_locked_1611566404604;
+    
+                    
+
+                    
+/**
+ * 
+ * 将字符串通过间隔符分隔成数组
+ * 
+ * @import is.empty
+ * 
+ * @param {String} target 字符串
+ * 
+ * @param {RegEx} splitRe 字符串分隔符
+ * 
+ * @scoped
+ * 
+ */
+
+function main(target , splitRe){
+
+    return target.split(splitRe).filter(filter) ;
+ }
+
+ function filter(target){
+
+    return !isEmpty(target) ;
+ }
+    
+                    return function(target , splitRe){
+    
+                        
+        if(!var_init_locked_1611566404604){
+
+            isEmpty = include('src::is.empty');
+
+            var_init_locked_1611566404604 = true ;
+        }
+        
+    
+                        return main.call(this , target , splitRe) ;
+                    } ;
+    
+                })();
+
+innerExports['src::is.object'] = (() =>{
+
+                    let isType;
+    
+                    let var_init_locked_1611566400702;
+    
+                    
+
+                    function main(data){
+
+        /**
+ * 
+ * 判定数据是否为对象类型
+ * 
+ * @import is.type
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @return {boolean} 如果给定值为对象类型则返回 true , 否则返回 false 
+ * 
+ */
+
+return typeof data === 'object' ;
+
+    }
+    
+                    return function(data){
+    
+                        
+        if(!var_init_locked_1611566400702){
+
+            isType = include('src::is.type');
+
+            var_init_locked_1611566400702 = true ;
+        }
+        
+    
+                        return main.call(this , data) ;
+                    } ;
+    
+                })();
+
+innerExports['src::object.value.set'] = (() =>{
+
+                    let isObject,split;
+    
+                    let var_init_locked_1611566409493;
+    
+                    
+
+                    /**
+ * 
+ * 设置对象的属性值
+ * 
+ * @import is.object
+ * 
+ * @import split from string.split
+ * 
+ * @param {object} target 目标对象
+ * 
+ * @param {string} key 属性名称
+ * 
+ * @param {mixed} value 属性值
+ * 
+ * @scoped
+ * 
+ */
+
+const splitRe = /\./;
+
+function main(target , key , value){
+
+    if(splitRe.test(key)){
+
+        let keys = split(key , splitRe) ;
+    
+        key = keys.pop();
+    
+        for(let key of keys){
+    
+            let data = target[key] ;
+    
+            if(!isObject(data)){
+    
+                data = target[key] = {} ;
+            }
+    
+            target = data ;
+        }
+    
+        target[key] = value ;
+    
+    }else{
+    
+        target[key] = value ;
+    }
+}   
+    
+                    return function(target , key , value){
+    
+                        
+        if(!var_init_locked_1611566409493){
+
+            isObject = include('src::is.object');
+split = include('src::string.split');
+
+            var_init_locked_1611566409493 = true ;
+        }
+        
+    
+                        return main.call(this , target , key , value) ;
+                    } ;
+    
+                })();
+
 
 
 
