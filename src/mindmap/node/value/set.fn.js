742f5ae49627b8b74d89f0ce2ad70b780f5e7430
陈治文 <cheng022074@qq.com>
2021-07-19 10:45:38 +0800

    支持节点设置一个对象值

--- src/mindmap/node/value/set.fn.js
+++ src/mindmap/node/value/set.fn.js
@@ -8,15 +8,37 @@
  * 
  * @import data from ..data scoped
  * 
- * @import setValue from ..sync.value.set scoped
+ * @import setValue from ..api.value.set scoped
  * 
- * @param {string} field 字段名称
+ * @import isObject from is.object.simple
+ * 
+ * @import set from .set scoped
+ * 
+ * @param {mixed} field 字段名称
  * 
  * @param {mixed} value 字段值
  * 
  * @param {mixed} [node] 节点
  * 
+ * @return {boolean} 如果节点信息修改成功则返回 true , 否则返回 false
+ * 
  */
+
+if(isObject(field)){
+
+    let names = Object.keys(fields),
+        isUpdate = false;
+
+    for(let name of names){
+
+        if(set(name , fields[name] , node)){
+
+            isUpdate = true ;
+        }
+    }
+
+    return isUpdate ;
+}
 
 let updatedNode = from(node),
     oldValue = updatedNode[field];
