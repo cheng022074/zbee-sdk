c37147544b011bbe964f59f9858c83d250c2f62b
陈治文 <cheng022074@qq.com>
2021-07-19 10:13:53 +0800

    Node API - 设置节点 API

---
+++ src/mindmap/node/api/value/set.fn.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * 设置或获取节点值
+ * 
+ * @import equals from data.equals
+ * 
+ * @import clone from data.clone
+ * 
+ * @import from from ....from scoped
+ * 
+ * @param {string} field 字段名称
+ * 
+ * @param {mixed} value 字段值
+ * 
+ * @param {mixed} [node] 节点
+ * 
+ */
+
+ node = from(node) ;
+
+ if(node){
+
+    let oldValue = node[field],
+        newValue = node[field] = clone(value);
+
+    return !equals(newValue , oldValue) ;
+ }
+
+return false ;
