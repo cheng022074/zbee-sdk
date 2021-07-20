e0f2e0054ccf177c03b550aebef199a68dc644b9
陈治文 <cheng022074@qq.com>
2021-07-20 10:17:00 +0800

    同步删除节点

--- src/mindmap/node/sync/delete.fn.js
+++ src/mindmap/node/sync/delete.fn.js
@@ -5,11 +5,28 @@
  * 
  * @import remove from ..api.delete scoped
  * 
+ * @import from from ..from scoped
+ * 
+ * @import getParentNode from ..parent scoped
+ * 
  * @param {mixed} node 节点
  * 
  * @return {boolean} 如果同步删除节点成功则返回 true , 否则返回 false
  * 
  */
 
-return !!remove(node) ;
+let deletedNode = from(node),
+    isLayout = false;
+
+if(deletedNode){
+
+    if(!getParentNode(deletedNode).hidden){
+
+        isLayout = true ;
+    }
+
+    remove(node) ;
+}
+
+return isLayout ;
 
