42c5349a0f6f66ceb49a4e09d4242b89f014938c
陈治文 <cheng022074@qq.com>
2021-07-20 10:22:13 +0800

    同步插入节点逻辑重构

--- src/mindmap/node/sync/insert.fn.js
+++ src/mindmap/node/sync/insert.fn.js
@@ -5,6 +5,10 @@
  * 
  * @import insert from ..api.insert scoped
  * 
+ * @import from from ..from scoped
+ * 
+ * @import getParentNode from ..parent scoped
+ * 
  * @param {mixed} node 需要插入的节点
  * 
  * @param {mixed} baseNode 参照节点
@@ -15,4 +19,30 @@
  * 
  */
 
-return !! insert(node , baseNode , region) ;
+let insertedNode = from(node),
+    isLayout = false;
+
+baseNode = from(baseNode) ;
+
+if(
+    insertedNode &&
+    (
+        !insertedNode.hidden ||
+        !getParentNode(insertedNode).hidden
+    )
+){
+
+    isLayout = true ;
+}
+
+if(baseNode){
+
+    node = insert(node , baseNode , region) ;
+
+    if(node && !node.hidden){
+
+        isLayout = true ;
+    }
+}
+
+return isLayout ;
