ce4caf5de074149c9b2b1001570a47955a1d477b
陈治文 <cheng022074@qq.com>
2021-07-27 11:00:17 +0800

    更新 ZBEE SDK

--- package/ok-web-app-core/index.dev.js
+++ package/ok-web-app-core/index.dev.js
@@ -702,6 +702,39 @@ return isType(data , 'function') && !data.__ZBEE_IS_CLASS__;
     
                 })();
 
+innerExports['src::is.defined'] = (() =>{
+
+                    
+    
+                    
+    
+                    
+
+                    function main(data){
+
+        /**
+ * 
+ * 判断给定数据是否定义
+ * 
+ * @param {mixed} data 检验数据
+ * 
+ * @return {boolean} 如果数据定义则返回 true , 否则返回 false
+ * 
+ */
+
+return data !== undefined ;
+
+    }
+    
+                    return function(data){
+    
+                        
+    
+                        return main.call(this , data) ;
+                    } ;
+    
+                })();
+
 innerExports['src::array.from'] = (() =>{
 
                     let isEmpty,isString;
