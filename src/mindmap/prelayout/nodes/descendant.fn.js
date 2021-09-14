7ef00d1159519bf4a015add657291bcbcd171c22
陈治文 <cheng022074@qq.com>
2021-09-14 10:16:02 +0800

    预布局 - 获取子孙节点

---
+++ src/mindmap/prelayout/nodes/descendant.fn.js
@@ -0,0 +1,41 @@
+
+/**
+ * 
+ * 获得子孙节点
+ * 
+ * @import from from mindmap.node.from scoped
+ * 
+ * @import getChildNodes from .child scoped
+ * 
+ * @param {mixed} node 脑图节点
+ * 
+ * @return {array} 子孙节点数组 
+ * 
+ */
+
+ function main(node){
+
+    return getDescendantNodes(from(node)) ;
+ }
+
+ function getDescendantNodes({
+     expanded,
+     children
+ }){
+
+    let result = [] ;
+
+    if(expanded){
+
+        for(let childNode of children){
+
+            result.push(childNode) ;
+
+            result.push(...getDescendantNodes(childNode , isExpanded)) ;
+        }
+
+        return result ;
+    }
+
+    return result ;
+ }
