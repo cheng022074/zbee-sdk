30af49737f7cae3eeabaeceaee1e4746247d4351
陈治文 <cheng022074@qq.com>
2021-11-16 14:47:47 +0800

    更新

--- package/xy/index.js
+++ package/xy/index.js
@@ -44156,6 +44156,121 @@ exports['src::mindmap.layout.pattern.logic.regions.layouted'] = (() => {
 
 })();
 
+exports['src::mindmap.layout.pattern.absolutely'] = (() => {
+
+    let getHeight, getWidth, setAnchorY, setRegionOffsetY, getAnchorY, getY, contains, from;
+
+    let var_init_locked_1637044778349;
+
+
+
+
+    /**
+     * 
+     * 绝对定位布局实现
+     * 
+     * @import getHeight from math.region.height
+     * 
+     * @import getWidth from math.region.width
+     * 
+     * @import setAnchorY from math.region.y.anchor
+     * 
+     * @import setRegionOffsetY from math.region.y.offset
+     * 
+     * @import getAnchorY from math.region.y.anchor
+     * 
+     * @import getY from math.region.y
+     * 
+     * @import contains from math.region.contains.x
+     * 
+     * @import from from math.region.from
+     * 
+     * @param {data.Record} node 布局节点
+     * 
+     */
+
+    function main(node) {
+
+        let me = this;
+
+        layout.call(me, node);
+
+        let {
+            width,
+            height,
+            padding
+        } = this,
+        region = {
+                top: 0,
+                bottom: height,
+                left: 0,
+                right: width
+            }, {
+                left,
+                right,
+                top,
+                bottom
+            } = padding,
+            regionHeight = getHeight(region),
+            mindmapWidth = getWidth(region) + left + right,
+            mindmapHeight = regionHeight + top + bottom,
+            offsetX = left,
+            offsetY = 0;
+
+        if (width > mindmapWidth) {
+
+            mindmapWidth = width;
+
+        }
+
+        if (height > mindmapHeight) {
+
+
+        } else {
+
+            offsetY = top;
+        }
+
+        return {
+            offset: {
+                x: offsetX,
+                y: offsetY
+            },
+            size: {
+                width: mindmapWidth,
+                height: mindmapHeight
+            }
+        };
+    }
+
+    function layout() {
+
+
+    }
+
+    return function(node) {
+
+
+        if (!var_init_locked_1637044778349) {
+
+            getHeight = include('src::math.region.height');
+            getWidth = include('src::math.region.width');
+            setAnchorY = include('src::math.region.y.anchor');
+            setRegionOffsetY = include('src::math.region.y.offset');
+            getAnchorY = include('src::math.region.y.anchor');
+            getY = include('src::math.region.y');
+            contains = include('src::math.region.contains.x');
+            from = include('src::math.region.from');
+
+            var_init_locked_1637044778349 = true;
+        }
+
+
+        return main.call(this, node);
+    };
+
+})();
+
 exports['src::mindmap.layout.positioner.logic.right'] = (() => {
 
     let init, getNode, Logic, generateLeftRegions, generateRightRegions, getLeftNodeAnchors, getRightNodeAnchors;
