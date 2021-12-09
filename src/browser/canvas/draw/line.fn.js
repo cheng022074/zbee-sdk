7c36b0b3212c88ea00ff21f965681593ca6a5b8b
陈治文 <cheng022074@qq.com>
2021-12-09 14:09:52 +0800

    画箭头设置

--- src/browser/canvas/draw/line.fn.js
+++ src/browser/canvas/draw/line.fn.js
@@ -19,6 +19,8 @@
  * 
  * @param {boolean} [config.clip = false] 是否为剪切路径
  * 
+ * @param {boolean} [config.arrow = false] 是否画箭头
+ * 
  * @param {object} [...config.styles] 画线样式
  * 
  */
@@ -34,6 +36,19 @@
     context.moveTo(...points.slice(0 , 2));
     
     context.lineTo(...points.slice(2));
+
+    if(arrow){
+
+      let headlen = 10,
+          angle = Math.atan2(points[3] - points[1] , points[2] - points[0]);
+
+      context.lineTo(points[2] - headlen * Math.cos(angle - Math.PI / 6) , points[3] - headlen * Math.sin(angle - Math.PI / 6));
+
+      context.moveTo(...points.slice(2));
+
+      context.lineTo(points[2] - headlen * Math.cos(angle + Math.PI / 6) , points[3] - headlen * Math.sin(angle + Math.PI / 6));
+    
+    }
     
     doEnd(context , clip) ;
  }
