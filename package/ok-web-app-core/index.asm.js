a99229cbf8d078dd56254d649a423c10712fd42a
陈治文 <cheng022074@qq.com>
2022-05-23 12:01:33 +0800

    增加打包配置

--- package/ok-web-app-core/index.asm.js
+++ package/ok-web-app-core/index.asm.js
@@ -4758,6 +4758,84 @@ return {
     
                 })();
 
+innerExports['src::function.call'] = (() =>{
+
+                    let isFunction,isObject,isArray,get,call;
+    
+                    let var_init_locked_1612162354781;
+    
+                    
+
+                    function main(target , ...args){
+
+        
+/**
+ * 
+ * 执行函数
+ * 
+ * @import is.function
+ * 
+ * @import isObject from is.object.simple
+ * 
+ * @import is.array
+ * 
+ * @import get from .get
+ * 
+ * @import call from .call
+ * 
+ * @param {mixed} target 函数或者函数描述
+ * 
+ * @param {array} [...args] 函数参数
+ * 
+ * @return {mixed} 函数的返回值
+ * 
+ */
+
+ if(isFunction(target)){
+
+    return target(...args) ;
+
+ }else if(isObject(target)){
+
+    let {
+        fn,
+        scope
+    } = target ;
+
+    get(fn , scope).apply(scope , args) ;
+
+ }else if(isArray(target)){
+
+    let targets = target ;
+
+    for(let target of targets){
+
+        call(target , ...args) ;
+    }
+ }
+
+    }
+    
+                    return function(target , ...args){
+    
+                        
+        if(!var_init_locked_1612162354781){
+
+            isFunction = include('src::is.function');
+isObject = include('src::is.object.simple');
+isArray = include('src::is.array');
+get = include('src::function.get');
+call = include('src::function.call');
+
+            var_init_locked_1612162354781 = true ;
+        }
+        
+    
+                        return main.call(this , target , ...args) ;
+                    } ;
+    
+                })();
+
 
 
 
