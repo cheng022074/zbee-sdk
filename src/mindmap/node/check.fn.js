24f50bd6141931a171cf514ac22d02a8de4b8905
陈治文 <cheng022074@qq.com>
2022-07-13 09:36:35 +0800

    勾选与取消勾选

---
+++ src/mindmap/node/check.fn.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 勾选节点
+ * 
+ * @param {object} node 节点
+ * 
+ */
+
+ from(node).checked = true ;
---
+++ src/mindmap/node/uncheck.fn.js
@@ -0,0 +1,9 @@
+/**
+ * 
+ * 取消勾选节点
+ * 
+ * @param {object} node 节点
+ * 
+ */
+
+from(node).checked = false ;
