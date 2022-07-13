9f654c6ed411df77f333845dc1cc1d5d1575a965
陈治文 <cheng022074@qq.com>
2022-07-13 09:24:01 +0800

    关闭选择模式

---
+++ src/mindmap/node/selectionMode/check/disable.fn.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * 关闭选择模式
+ * 
+ * @import from from mindmap.node.from scoped
+ * 
+ * @import getDescendantNodes from mindmap.nodes.descendant scoped
+ * 
+ * @param {mixed} node 节点
+ * 
+ */  
+
+ node = from(node) ;
+
+ if(node.checkSelectionMode){
+
+    node.checkSelectionMode = false ;
+
+    getDescendantNodes(node).forEach(node => node.checkSelectionMode = false) ;
+
+    return true ;
+ }
+
+ return false ;
+
